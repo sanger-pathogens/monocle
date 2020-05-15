57f4014746b6d31dfc7eaa24fb354c981e8aa70e
Gareth Peat <gp16@sanger.ac.uk>
2020-05-15 16:25:15 +0100

    Add csrf exemption for GraphQL API

--- api-django/juno/urls.py
+++ api-django/juno/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
-    url(r"^graphql$", GraphQLView.as_view(graphiql=True)),
+    url(r"^graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
