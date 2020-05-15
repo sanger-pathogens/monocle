5dc0ff38b8a9a7ca5621f9a77da3ba1fe58fdf6c
Gareth Peat <gp16@sanger.ac.uk>
2020-05-15 15:03:52 +0100

    Fix max_length attribute

--- api-django/juno/api/models.py
+++ api-django/juno/api/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 
 class Institution(models.Model):
-    name = models.CharField(256, primary_key=True)
-    country = models.CharField(256)
+    name = models.CharField(max_length=256, primary_key=True)
+    country = models.CharField(max_length=256)
 
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
-    first_name = models.CharField(256)
-    last_name = models.CharField(256)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
     affiliations = models.ManyToManyField(
         Institution, related_name="affiliated_members"
     )
 
 
 class Sample(models.Model):
-    lane_id = models.CharField(256, primary_key=True)
-    sample_id = models.CharField(256, unique=True)
-    public_name = models.CharField(256, unique=True)
+    lane_id = models.CharField(max_length=256, primary_key=True)
+    sample_id = models.CharField(max_length=256, unique=True)
+    public_name = models.CharField(max_length=256, unique=True)
     submitting_institution = models.ForeignKey(
         Institution, on_delete=models.CASCADE, related_name="samples"
     )
