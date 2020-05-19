e014d72d3355b306de9e6544f86655ac92595628
Gareth Peat <gp16@sanger.ac.uk>
2020-05-19 17:06:15 +0100

    Add container runtime settings for UI

---
+++ ui/public/settings.js
@@ -0,0 +1,17 @@
+/*
+ * Runtime settings
+ *
+ * Because we transpile the React app at Docker image build time,
+ * it's not possible to easily pass environment variables for
+ * configuration at container runtime, since we only have static
+ * files to be served.
+ *
+ * This file follows the dynamic approach described at:
+ *   https://levelup.gitconnected.com/handling-multiple-environments-in-react-with-docker-543762989783
+ *
+ * To overwrite the settings at container runtime, simply mount
+ * a new settings.js file to eg. /usr/share/nginx/html
+ */
+window.env = {
+  GRAPHQL_API_URL: "http://localhost:8000/graphql/",
+};
