e9bb0293b516a82c08b88d09b9061287bfe2b4a8
kathryn1995 <km22@sanger.ac.uk>
2020-05-22 16:26:12 +0100

    Adding host status field into sample table

--- api/juno/api/models.py
+++ api/juno/api/models.py
@@ -31,6 +31,7 @@ class Sample(models.Model):
     lane_id = models.CharField(max_length=256, primary_key=True)
     sample_id = models.CharField(max_length=256, unique=True)
     public_name = models.CharField(max_length=256, unique=True)
+    host_status = models.CharField(max_length=256)
     submitting_institution = models.ForeignKey(
         Institution, on_delete=models.CASCADE, related_name="samples"
     )
