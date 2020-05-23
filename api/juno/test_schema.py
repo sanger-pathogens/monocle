c85980b89a055e5079e235015a1e598163ed0687
Gareth Peat <gp16@sanger.ac.uk>
2020-05-23 18:10:43 +0100

    Remove early tests (now more granular coverage)

--- api/juno/test_schema.py
+++ api/juno/test_schema.py
@@ -5,71 +5,6 @@ from juno.schema import schema
 from juno.api.models import Sample, Institution
 
 
-class EmptyDatabaseSchemaTest(GraphQLTestCase):
-    GRAPHQL_SCHEMA = schema
-
-    def test_query_samples(self):
-        response = self.query(
-            """
-            query {
-                samples {
-                    laneId
-                }
-            }
-            """,
-        )
-
-        # query ok?
-        self.assertResponseNoErrors(response)
-
-        # expected content?
-        actual_content = json.loads(response.content)
-        expected_content = {"data": {"samples": []}}
-        self.assertDictEqual(
-            actual_content, expected_content, msg="Unexpected data in response"
-        )
-
-
-class NonEmptyDatabaseSchemaTest(GraphQLTestCase):
-    GRAPHQL_SCHEMA = schema
-    fixtures = [
-        "institutions.json",
-        "samples.json",
-    ]  # alternatively, could generate in setUp() function
-
-    def test_query_samples(self):
-        response = self.query(
-            """
-            query {
-                samples {
-                    laneId
-                }
-            }
-            """,
-        )
-
-        # query ok?
-        self.assertResponseNoErrors(response)
-
-        # expected content?
-        actual_content = json.loads(response.content)
-        self.assertIn(
-            "samples",
-            actual_content["data"],
-            msg="Expected samples field in response",
-        )
-        self.assertGreater(
-            len(actual_content["data"]["samples"]),
-            0,
-            msg="Expected data in response, got none",
-        )
-        self.assertIn(
-            "laneId",
-            actual_content["data"]["samples"][0],
-            msg="Expected laneId field in each sample response",
-        )
-
-
 class APITestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
 
@@ -230,4 +165,3 @@ class InstitutionsQueryTestCase(APITestCase):
             "country",
             expected_value=self.institution.country,
         )
-
