57280d0111d4d6797ed28c4da5eda29973887715
Gareth Peat <gp16@sanger.ac.uk>
2020-05-23 17:57:17 +0100

    Refactor samples query tests

--- api/juno/test_schema.py
+++ api/juno/test_schema.py
@@ -70,15 +70,77 @@ class NonEmptyDatabaseSchemaTest(GraphQLTestCase):
         )
 
 
-class SamplesQueryTest(GraphQLTestCase):
+class APITestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
+
+    # common messages
+    MSG_ROOT_NOT_AN_OBJECT = "API response should be a JSON object"
+    MSG_NO_DATA = "API response has no root data field"
+    MSG_FIELD_MISSING = "API response should contain field"
     MSG_VALUE_MISMATCH = "Value in API response differs from value in database"
+    MSG_VALUE_EXPECTED_LIST = "Value in API response should be a list type"
     MSG_VALUE_EXPECTED_NONPRIMITIVE_TYPE = (
-        "Value in API response should be an object"
+        "Value in API response should be a non-primitive type (dict)"
     )
     MSG_VALUE_EXPECTED_STRING_TYPE = "Value in API response should be a string"
-    MSG_MISSING_FIELD = "API response should contain field"
+    MSG_EXPECTED_NONEMPTY_LIST = "Expected non-empty list"
 
+    def make_and_validate_query(self, queryname, subquery):
+        # make query
+        response = self.query(
+            """
+            query {
+                %s {
+                    %s
+                }
+            }
+            """
+            % (queryname, subquery,),
+        )
+
+        # query ok?
+        self.assertResponseNoErrors(response)
+
+        # expected content?
+        content = json.loads(response.content)
+        self.assertIsInstance(content, dict, msg=self.MSG_ROOT_NOT_AN_OBJECT)
+        self.assertIn("data", content, msg=self.MSG_NO_DATA)
+
+        # has the query?
+        self.assertIn(
+            queryname, content["data"], msg=self.MSG_FIELD_MISSING,
+        )
+        value = content["data"][queryname]
+
+        # return for further checks
+        return value
+
+    def validate_field(self, parent, fieldname, expected_value=None):
+        # parent in response an object?
+        self.assertIsInstance(
+            parent, dict, msg=self.MSG_VALUE_EXPECTED_NONPRIMITIVE_TYPE
+        )
+
+        # parent has the field?
+        self.assertIn(fieldname, parent, msg=self.MSG_FIELD_MISSING)
+
+        # potentially check the value, but return for further checks
+        value = parent[fieldname]
+        if expected_value is not None:
+            self.assertEqual(value, expected_value, self.MSG_VALUE_MISMATCH)
+
+        return value
+
+    def validate_non_empty_list(self, expected_list):
+        self.assertIsInstance(
+            expected_list, list, msg=self.MSG_VALUE_EXPECTED_LIST
+        )
+        self.assertGreater(
+            len(expected_list), 0, msg=self.MSG_EXPECTED_NONEMPTY_LIST
+        )
+
+
+class SamplesQueryTestCase(APITestCase):
     def setUp(self):
         # put something in the db
         self.institution = Institution.objects.create(
@@ -91,76 +153,44 @@ class SamplesQueryTest(GraphQLTestCase):
             submitting_institution=self.institution,
         )
 
-    def verify_field_present(self, field, subquery):
-        # make query
-        response = self.query(
-            """
-            query {
-                samples {
-                    %s
-                }
-            }
-            """
-            % (subquery,),
-        )
-
-        # query ok?
-        self.assertResponseNoErrors(response)
-
-        # expected content?
-        actual_content = json.loads(response.content)
-
-        # has the query?
-        self.assertIn(
-            "samples", actual_content["data"], msg=self.MSG_MISSING_FIELD,
-        )
-        self.assertEqual(
-            len(actual_content["data"]["samples"]),
-            1,
-            msg="Expected one sample",
-        )
-
-        # has the field?
-        sample = actual_content["data"]["samples"][0]
-        self.assertIn(
-            field, sample, msg=self.MSG_MISSING_FIELD,
-        )
-        value = sample[field]
-
-        # return field value for further checks
-        return value
-
     def test_has_field_lane_id(self):
-        value = self.verify_field_present("laneId", "laneId")
-        self.assertEqual(
-            value, self.sample.lane_id, msg=self.MSG_VALUE_MISMATCH
+        query_value = self.make_and_validate_query("samples", "laneId")
+        self.validate_non_empty_list(query_value)
+        self.validate_field(
+            query_value[0], "laneId", expected_value=self.sample.lane_id
         )
 
     def test_has_field_sample_id(self):
-        value = self.verify_field_present("sampleId", "sampleId")
-        self.assertEqual(
-            value, self.sample.sample_id, msg=self.MSG_VALUE_MISMATCH
+        query_value = self.make_and_validate_query("samples", "sampleId")
+        self.validate_non_empty_list(query_value)
+        self.validate_field(
+            query_value[0], "sampleId", expected_value=self.sample.sample_id
         )
 
     def test_has_field_public_name(self):
-        value = self.verify_field_present("publicName", "publicName")
-        self.assertEqual(
-            value, self.sample.public_name, msg=self.MSG_VALUE_MISMATCH
+        query_value = self.make_and_validate_query("samples", "publicName")
+        self.validate_non_empty_list(query_value)
+        self.validate_field(
+            query_value[0],
+            "publicName",
+            expected_value=self.sample.public_name,
         )
 
     def test_has_field_submitting_institution(self):
-        value = self.verify_field_present(
-            "submittingInstitution",
+        query_value = self.make_and_validate_query(
+            "samples",
             """
             submittingInstitution {
                 name
             }
             """,
         )
-        self.assertIsInstance(
-            value, dict, msg=self.MSG_VALUE_EXPECTED_NONPRIMITIVE_TYPE
+        self.validate_non_empty_list(query_value)
+        submitting_institution = self.validate_field(
+            query_value[0], "submittingInstitution"
         )
-        self.assertIn("name", value, msg=self.MSG_MISSING_FIELD)
-        self.assertEqual(
-            value["name"], self.institution.name, msg=self.MSG_VALUE_MISMATCH
+        self.validate_field(
+            submitting_institution,
+            "name",
+            expected_value=self.institution.name,
         )
