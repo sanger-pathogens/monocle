c728f531071f90dfba5183182679ac08e96e3685
Gareth Peat <gp16@sanger.ac.uk>
2020-05-27 14:36:25 +0100

    Add test case for protected query

--- api/juno/test_schema.py
+++ api/juno/test_schema.py
@@ -1,13 +1,88 @@
 import json
-from graphene_django.utils.testing import GraphQLTestCase
+from django.test import TestCase
 
 from juno.schema import schema
-from juno.api.models import Sample, Institution
+from juno.api.models import Sample, Institution, User
+
+
+class GraphQLTestCase(TestCase):
+    """
+    Adapted from `graphene_django.utils.testing.GraphQLTestCase`
+    """
+
+    # URL to graphql endpoint
+    GRAPHQL_URL = "/graphql/"
+
+    def query(
+        self,
+        query,
+        op_name=None,
+        input_data=None,
+        variables=None,
+        headers=None,
+    ):
+        """
+        Args:
+            query (string)    - GraphQL query to run
+            op_name (string)  - If the query is a mutation or named query, you must
+                                supply the op_name.  For annon queries ("{ ... }"),
+                                should be None (default).
+            input_data (dict) - If provided, the $input variable in GraphQL will be set
+                                to this value. If both ``input_data`` and ``variables``,
+                                are provided, the ``input`` field in the ``variables``
+                                dict will be overwritten with this value.
+            variables (dict)  - If provided, the "variables" field in GraphQL will be
+                                set to this value.
+            headers (dict)    - If provided, the headers in POST request to GRAPHQL_URL
+                                will be set to this value.
+        Returns:
+            Response object from client
+        """
+        body = {"query": query}
+        if op_name:
+            body["operationName"] = op_name
+        if variables:
+            body["variables"] = variables
+        if input_data:
+            if variables in body:
+                body["variables"]["input"] = input_data
+            else:
+                body["variables"] = {"input": input_data}
+        if headers:
+            resp = self.client.post(
+                self.GRAPHQL_URL,
+                json.dumps(body),
+                content_type="application/json",
+                **headers
+            )
+        else:
+            resp = self.client.post(
+                self.GRAPHQL_URL,
+                json.dumps(body),
+                content_type="application/json",
+            )
+        return resp
+
+    def assertResponseNoErrors(self, resp):
+        """
+        Assert that the call went through correctly. 200 means the syntax is ok, if there are no `errors`,
+        the call was fine.
+        :resp HttpResponse: Response
+        """
+        self.assertEqual(resp.status_code, 200)
+        content = json.loads(resp.content)
+        self.assertNotIn("errors", list(content.keys()))
+
+    def assertResponseHasErrors(self, resp):
+        """
+        Assert that the call was failing. Take care: Even with errors, GraphQL returns status 200!
+        :resp HttpResponse: Response
+        """
+        content = json.loads(resp.content)
+        self.assertIn("errors", list(content.keys()))
 
 
 class APITestCase(GraphQLTestCase):
-    GRAPHQL_SCHEMA = schema
-
     # common messages
     MSG_ROOT_NOT_AN_OBJECT = "API response should be a JSON object"
     MSG_NO_DATA = "API response has no root data field"
@@ -19,6 +94,29 @@ class APITestCase(GraphQLTestCase):
     )
     MSG_VALUE_EXPECTED_STRING_TYPE = "Value in API response should be a string"
     MSG_EXPECTED_NONEMPTY_LIST = "Expected non-empty list"
+
+    def validate_successful(self, response):
+        # query ok?
+        self.assertResponseNoErrors(response)
+
+        # expected content?
+        content = json.loads(response.content)
+        self.assertIsInstance(content, dict, msg=self.MSG_ROOT_NOT_AN_OBJECT)
+        self.assertIn("data", content, msg=self.MSG_NO_DATA)
+
+        # return data for further checks
+        return content["data"]
+
+    def validate_unsuccessful(self, response):
+        # query ok?
+        self.assertResponseHasErrors(response)
+
+        # expected content?
+        content = json.loads(response.content)
+        self.assertIsInstance(content, dict, msg=self.MSG_ROOT_NOT_AN_OBJECT)
+
+        # return content for further checks
+        return content
 
     def make_and_validate_query(self, queryname, subquery):
         # make query
@@ -33,19 +131,13 @@ class APITestCase(GraphQLTestCase):
             % (queryname, subquery,),
         )
 
-        # query ok?
-        self.assertResponseNoErrors(response)
-
-        # expected content?
-        content = json.loads(response.content)
-        self.assertIsInstance(content, dict, msg=self.MSG_ROOT_NOT_AN_OBJECT)
-        self.assertIn("data", content, msg=self.MSG_NO_DATA)
+        data = self.validate_successful(response)
 
         # has the query?
         self.assertIn(
-            queryname, content["data"], msg=self.MSG_FIELD_MISSING,
+            queryname, data, msg=self.MSG_FIELD_MISSING,
         )
-        value = content["data"][queryname]
+        value = data[queryname]
 
         # return for further checks
         return value
@@ -72,6 +164,29 @@ class APITestCase(GraphQLTestCase):
         )
         self.assertGreater(
             len(expected_list), 0, msg=self.MSG_EXPECTED_NONEMPTY_LIST
+        )
+
+    def login(self, email, password):
+        self.query(
+            """
+            mutation {
+                tokenAuth(email:"bob@bob.com", password: "bobsicle") {
+                    token
+                }
+            }
+            """,
+        )
+        # TODO: return token
+
+    def logout(self):
+        self.query(
+            """
+            mutation {
+                deleteTokenCookie {
+                    deleted
+                }
+            }
+            """,
         )
 
 
@@ -127,7 +242,9 @@ class SamplesQueryTestCase(APITestCase):
     def test_has_field_host_status(self):
         samples = self.make_and_validate_samples_query("hostStatus")
         self.validate_field(
-            samples[0], "hostStatus", expected_value="SKIN_AND_SOFT_TISSUE_INFECTION",
+            samples[0],
+            "hostStatus",
+            expected_value="SKIN_AND_SOFT_TISSUE_INFECTION",
         )
 
     def test_has_field_submitting_institution(self):
@@ -163,7 +280,7 @@ class InstitutionsQueryTestCase(APITestCase):
         # check non-empty
         self.validate_non_empty_list(institutions)
 
-        # return samples
+        # return institutions
         return institutions
 
     def test_has_field_name(self):
@@ -179,3 +296,36 @@ class InstitutionsQueryTestCase(APITestCase):
             "country",
             expected_value=self.institution.country,
         )
+
+
+class MeQueryTestCase(APITestCase):
+    def setUp(self):
+        # put something in the db
+        self.user = User.objects.create(
+            email="bob@bob.com", first_name="Bob", last_name="Bobbity",
+        )
+        self.user.set_password("bobsicle")
+        self.user.save()
+
+    def make_me_query(self, subquery):
+        # call api
+        return self.query(
+            """
+            query {
+                me {
+                    %s
+                }
+            }
+            """
+            % subquery,
+        )
+
+    def test_has_field_email_when_logged_in(self):
+        # auth
+        self.login(self.user.email, self.user.password)
+
+        # query
+        response = self.make_me_query("email")
+        data = self.validate_successful(response)
+        me = self.validate_field(data, "me")
+        self.validate_field(me, "email", expected_value=self.user.email)
