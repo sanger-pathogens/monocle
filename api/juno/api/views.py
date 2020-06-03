b7cfcdd014abdb76c6d10cce957d80a191f4ce18
kathryn1995 <km22@sanger.ac.uk>
2020-06-03 16:22:42 +0100

    Add error if file being downloaded doesn't exist

--- api/juno/api/views.py
+++ api/juno/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import StreamingHttpResponse, HttpResponse
+from django.http import StreamingHttpResponse, HttpResponse, Http404
 from wsgiref.util import FileWrapper
 import os
 import mimetypes
@@ -17,11 +17,11 @@ def download_file(request, laneId):
     archive_file = lane_dir + "tar.gz"
     make_tarfile(archive_file, lane_dir)
     response = stream_file(archive_file)
+    os.remove(archive_file)
     return response
 
 
 def filter_file(thing):
-    print("{thing.name}".format(thing=thing))
     if thing.name in [
         "31663_7#113",
         "31663_7#113/31663_7#113_1.fastq.gz",
@@ -35,13 +35,16 @@ def filter_file(thing):
 
 
 def make_tarfile(output_filename, source_dir):
-    with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(
-            source_dir,
-            arcname=os.path.basename(source_dir),
-            recursive=True,
-            filter=filter_file,
-        )
+    if os.path.exists(source_dir):
+        with tarfile.open(output_filename, "w:gz") as tar:
+            tar.add(
+                source_dir,
+                arcname=os.path.basename(source_dir),
+                recursive=True,
+                filter=filter_file,
+            )
+    else:
+        raise Http404
 
 
 def stream_file(file_path):
