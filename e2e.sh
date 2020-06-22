7927f27c5de7ceda9637aaf5b50fe0eb8702e2fc
Gareth Peat <gp16@sanger.ac.uk>
2020-06-22 11:06:00 +0100

    Restrict e2e ssh key permissions further

--- e2e.sh
+++ e2e.sh
@@ -2,9 +2,23 @@
 
 # Create a key pair for communication between cypress and api,
 # needed for seeding the database between tests.
-ssh-keygen -t rsa -b 4096 -C "root@monocle.com" -f e2e/ssh/id_rsa -q -N ""
-chmod 400 e2e/ssh/id_rsa
-chmod 400 e2e/ssh/id_rsa.pub
+mkdir e2e-ssh
+ssh-keygen -t rsa -b 4096 -C "root@monocle.com" -f e2e-ssh/id_rsa -q -N ""
+
+# Create directories to mount
+mkdir e2e-ssh/cypress
+mkdir e2e-ssh/api
+
+# Move and rename keys
+mv e2e/cypress-ssh-config e2e-ssh/cypress/config
+mv e2e-ssh/id_rsa e2e-ssh/cypress/id_rsa
+mv e2e-ssh/id_rsa.pub e2e-ssh/api/authorized_keys
+
+# Set permissions
+chmod 600 e2e-ssh/cypress/id_rsa
+chmod 600 e2e-ssh/api/authorized_keys
+chmod 600 e2e-ssh/cypress
+chmod 600 e2e-ssh/api
 
 # Run the end-to-end tests.
 docker-compose -f docker-compose.e2e.yml build
