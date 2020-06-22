8640d410e361fabe24ca0150256f88defbd9d34d
Gareth Peat <gp16@sanger.ac.uk>
2020-06-22 09:34:45 +0100

    Change permissions on ssh key files

--- e2e.sh
+++ e2e.sh
@@ -3,6 +3,8 @@
 # Create a key pair for communication between cypress and api,
 # needed for seeding the database between tests.
 ssh-keygen -t rsa -b 4096 -C "root@monocle.com" -f e2e/ssh/id_rsa -q -N ""
+chmod 400 e2e/ssh/id_rsa
+chmod 400 e2e/ssh/id_rsa.pub
 
 # Run the end-to-end tests.
 docker-compose -f docker-compose.e2e.yml build
