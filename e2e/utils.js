2ff4a419a17c4ad9d5859ed3d5f86cd6e312e6f0
Gareth Peat <gp16@sanger.ac.uk>
2020-06-23 20:35:32 +0100

    Tidy up e2e tests with loadDatabaseProfile func

---
+++ e2e/utils.js
@@ -0,0 +1,29 @@
+// it is possible that tests fail due to an slow API request,
+// but we wait for the following amount of time, to hopefully
+// mitigate this
+export const API_WAIT_MS = 1000;
+
+// object to make allowed database profiles more obvious
+export const DB_PROFILES = {
+  EMPTY: "empty",
+  SMALL: "small",
+};
+
+// use custom django management command `e2e` to change
+// database content via python and return database content
+// to test in javascript via stdout
+export const loadDatabaseProfile = (profileName) =>
+  cy
+    .exec(`./run-on-api.sh python manage.py e2e ${profileName}`)
+    .then(({ code, stdout, stderr }) => {
+      // check db update ok
+      let data;
+      if (code === 0) {
+        data = JSON.parse(stdout);
+      } else {
+        cy.task("log", stderr);
+      }
+
+      // return database data for test
+      return data;
+    });
