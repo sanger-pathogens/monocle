4aa21285efcbe5c1682b3cc3d41bdd303caa7bb7
Gareth Peat <gp16@sanger.ac.uk>
2020-06-23 19:40:11 +0100

    Add e2e test with dynamic database

--- e2e/integration/spec.js
+++ e2e/integration/spec.js
@@ -1,3 +1,8 @@
+// it is possible that tests fail due to an slow API request,
+// but we wait for the following amount of time, to hopefully
+// mitigate this
+const API_WAIT_MS = 1000;
+
 it("loads the page", () => {
   cy.visit("/");
 
@@ -11,20 +16,6 @@ it("loads the react app", () => {
   // body should contain two tables, titled `Institutions` and `Samples`
   cy.get("body").should("contain", "Institutions");
   cy.get("body").should("contain", "Samples");
-});
-
-it("loads the institutions table", () => {
-  cy.visit("/");
-
-  // body should contain data from the `institutions.json` fixture
-  cy.get("body").should("contain", "National Reference Laboratories");
-});
-
-it("loads the samples table", () => {
-  cy.visit("/");
-
-  // body should contain data from the `samples.json` fixture
-  cy.get("body").should("contain", "31663_7#113");
 });
 
 it("is possible to connect to api", () => {
@@ -49,5 +40,29 @@ it("is possible clear the database", () => {
   cy.title().should("eq", "Monocle");
 
   // body should NOT contain data from the `samples.json` fixture
-  cy.get("body").should("not.contain", "31663_7#113");
+  // cy.get("body").should("not.contain", "31663_7#113");
+  cy.get("body").contains("31663_7#113").should("not.exist");
+});
+
+it("is possible retrieve seeded data from the database", () => {
+  cy.exec("./run-on-api.sh python manage.py e2e small").then(
+    ({ code, stdout, stderr }) => {
+      // check db update ok
+      let data;
+      if (code === 0) {
+        data = JSON.parse(stdout);
+      } else {
+        cy.task("log", stderr);
+      }
+
+      // test
+      if (data) {
+        cy.visit("/");
+        cy.wait(API_WAIT_MS);
+        data.sample.forEach(({ lane_id }) => {
+          cy.get("body").contains(lane_id).should("exist");
+        });
+      }
+    }
+  );
 });
