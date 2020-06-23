264d3804a5718b9d3e8d39bed746687d0f143d5d
Gareth Peat <gp16@sanger.ac.uk>
2020-06-23 16:04:50 +0100

    Remove docker root user on cypress

--- e2e/cypress-entrypoint.sh
+++ e2e/cypress-entrypoint.sh
@@ -5,7 +5,7 @@ echo "---------- cypress-entrypoint start ----------"
 whoami
 ls -l /root/.ssh
 chown -R $UID:$UID /root/.ssh
-apt install -y ssh-askpass
+ls -l /root/.ssh
 cypress run
 
 echo "---------- cypress-entrypoint end ----------"
