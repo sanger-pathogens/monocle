a7f38d01e91add8b1f89037278be82abc4daac0e
Gareth Peat <gp16@sanger.ac.uk>
2020-06-24 17:03:03 +0100

    Remove unneeded routing

--- ui/src/AuthenticatedApp.js
+++ ui/src/AuthenticatedApp.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
 
 import PageHome from "./components/PageHome";
-import PageLogin from "./components/PageLogin";
-import PageMe from "./components/PageMe";
-import history from "./history";
 import { UserProvider } from "./user";
 
 const AuthenticatedApp = () => (
   <UserProvider>
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={PageHome} />
-        <Route exact path="/me" component={PageMe} />
-        {/* <Route path="*" component={Page404} /> */}
-      </Switch>
-    </Router>
+    <PageHome />
   </UserProvider>
 );
 
