2e769cea7b8fc3c67014e33226e1edd15916156c
Gareth Peat <gp16@sanger.ac.uk>
2020-06-24 16:46:46 +0100

    Remove AuthRoute

--- ui/src/AuthenticatedApp.js
+++ ui/src/AuthenticatedApp.js
@@ -4,18 +4,19 @@ import { Router, Switch, Route } from "react-router-dom";
 import PageHome from "./components/PageHome";
 import PageLogin from "./components/PageLogin";
 import PageMe from "./components/PageMe";
-import AuthRoute from "./components/AuthRoute";
 import history from "./history";
+import { UserProvider } from "./user";
 
 const AuthenticatedApp = () => (
-  <Router history={history}>
-    <Switch>
-      <Route exact path="/" component={PageHome} />
-      <Route exact path="/login" component={PageLogin} />
-      <AuthRoute exact path="/me" component={PageMe} />
-      {/* <Route path="*" component={Page404} /> */}
-    </Switch>
-  </Router>
+  <UserProvider>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={PageHome} />
+        <Route exact path="/me" component={PageMe} />
+        {/* <Route path="*" component={Page404} /> */}
+      </Switch>
+    </Router>
+  </UserProvider>
 );
 
 export default AuthenticatedApp;
