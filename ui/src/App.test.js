ed3a363a70fb18049be4ef2adf3b8434ce7c0b67
Gareth Peat <gp16@sanger.ac.uk>
2020-06-25 17:39:17 +0100

    Get ui tests passing (though with act warning)

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForElement } from "@testing-library/react";
+
+import MockProviders from "./test-utils/MockProviders";
 import App from "./App";
 
 // TODO: write some more meaningful front-end only tests;
 //       the following are just to get ci set up
 
-test("renders text institutions", () => {
-  const { getByText } = render(<App />);
-  const maybeElement = getByText(/institutions/i);
-  expect(maybeElement).toBeInTheDocument();
+test("renders text institutions", async () => {
+  const { getByText } = render(
+    <MockProviders isInitiallyLoggedIn={true}>
+      <App />
+    </MockProviders>
+  );
+  // const maybeElement = getByText(/institutions/i);
+  // expect(maybeElement).toBeInTheDocument();
+  await waitForElement(() => getByText(/institutions/i));
 });
 
-test("renders text samples", () => {
-  const { getByText } = render(<App />);
-  const maybeElement = getByText(/samples/i);
-  expect(maybeElement).toBeInTheDocument();
+test("renders text samples", async () => {
+  const { getByText } = render(
+    <MockProviders isInitiallyLoggedIn={true}>
+      <App />
+    </MockProviders>
+  );
+  // await wait(0);
+  // const maybeElement = getByText(/samples/i);
+  // expect(maybeElement).toBeInTheDocument();
+  await waitForElement(() => getByText(/samples/i));
 });
