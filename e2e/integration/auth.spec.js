4ec1195467e5d3efa693d1f9ae4a2cb1d0b3a473
Gareth Peat <gp16@sanger.ac.uk>
2020-06-29 11:44:03 +0100

    Add logout test and clear cookies, localStorage

--- e2e/integration/auth.spec.js
+++ e2e/integration/auth.spec.js
@@ -1,16 +1,26 @@
 import { API_WAIT_MS, DB_PROFILES, loadDatabaseProfile } from "../utils";
 
 describe("authentication", () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
   it("exists", () => {
     cy.visit("/");
+
+    // credentials form?
     cy.get("input[type=email]").should("exist");
     cy.get("input[type=password]").should("exist");
   });
 
   it("supports login", () => {
     loadDatabaseProfile(DB_PROFILES.SMALL).then((db) => {
+      cy.visit("/");
+
       // credentials
-      const email = db.user[0].email;
+      const user = db.user[0];
+      const { email, first_name, last_name } = user;
       const password = email.split("@")[0];
 
       // fill in credentials
@@ -23,6 +33,38 @@ describe("authentication", () => {
 
       // sample table present?
       cy.get(`table#sampleTable`).should("exist");
+
+      // user's name present?
+      cy.get("body").should("contain", `${first_name} ${last_name}`);
+    });
+  });
+
+  it("supports logout", () => {
+    loadDatabaseProfile(DB_PROFILES.SMALL).then((db) => {
+      cy.visit("/");
+
+      // credentials
+      const user = db.user[0];
+      const { email, first_name, last_name } = user;
+      const password = email.split("@")[0];
+
+      // fill in credentials
+      cy.get("input[type=email]").type(email);
+      cy.get("input[type=password]").type(password);
+      cy.get("button[type=submit]").click();
+
+      // await logged in page
+      cy.wait(API_WAIT_MS);
+
+      // logout
+      cy.contains("Logout").click();
+
+      // await login page again
+      cy.wait(API_WAIT_MS);
+
+      // credentials form?
+      cy.get("input[type=email]").should("exist");
+      cy.get("input[type=password]").should("exist");
     });
   });
 });
