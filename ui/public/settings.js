6782fb8edf321a738c84d3ba3d699e210291bfe1
Gareth Peat <gp16@sanger.ac.uk>
2020-06-29 10:09:09 +0100

    Enable auth in dev

--- ui/public/settings.js
+++ ui/public/settings.js
@@ -17,5 +17,5 @@ const baseUrl = "http://localhost:8000/";
 window.env = {
   GRAPHQL_API_URL: `${baseUrl}graphql/`,
   DOWNLOAD_ROOT_URL: `${baseUrl}SampleDownload/`,
-  USE_AUTHENTICATION: false,
+  USE_AUTHENTICATION: true,
 };
