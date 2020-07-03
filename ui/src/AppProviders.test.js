aff34e50cf920ecd50f98d34b925c77dabc6a8d5
Gareth Peat <gp16@sanger.ac.uk>
2020-07-03 14:40:36 +0100

    Add AppProviders.test.js

---
+++ ui/src/AppProviders.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { renderHook } from "@testing-library/react-hooks";
+
+import AppProviders from "./AppProviders";
+import { useAuth } from "./auth";
+import { useUser } from "./user";
+
+describe("AppProviders", () => {
+  // helper
+  const wrapper = ({ children }) => <AppProviders>{children}</AppProviders>;
+
+  it("should provide AuthContext", () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper,
+    });
+
+    expect(result.current).toHaveProperty("isLoggedIn");
+    expect(result.current).toHaveProperty("isLoading");
+    expect(result.current).toHaveProperty("login");
+    expect(result.current).toHaveProperty("logout");
+  });
+
+  it("should provide UserContext", () => {
+    const { result } = renderHook(() => useUser(), {
+      wrapper,
+    });
+
+    expect(result.current).toHaveProperty("user");
+  });
+});
