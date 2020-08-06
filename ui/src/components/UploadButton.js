18c2ada344b8fe0e1884c8c3cd6d0938e6cb38e6
kathryn1995 <km22@sanger.ac.uk>
2020-08-06 08:34:29 +0100

    Add changes to upload button

--- ui/src/components/UploadButton.js
+++ ui/src/components/UploadButton.js
@@ -1,30 +1,48 @@
-import React, { Component } from "react";
-import Dropzone from "react-dropzone";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
-class UploadButton extends Component {
-  onDrop = (acceptedFiles) => {
-    console.log(acceptedFiles);
-  };
+const UploadButton = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
 
-  render() {
-    return (
-      <div className="text-center mt-5">
-        <Dropzone
-          onDrop={this.onDrop}
-          accept="application/vnd.ms-excel, text/tab-separated-values, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        >
-          {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              {!isDragActive && "Click here or drop a file to upload!"}
-              {isDragActive && !isDragReject && "Drop here!"}
-              {isDragReject && "File type not accepted, sorry!"}
-            </div>
-          )}
-        </Dropzone>
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+
+    // Convert to json
+    // Query
+    // Load new page
+  }, []);
+
+  const {
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    isDragReject,
+    maxFiles = 1,
+  } = useDropzone({
+    onDrop,
+    accept:
+      "application/vnd.ms-excel, text/tab-separated-values, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  return (
+    <div className="container text-center mt-5">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {!isDragActive && "Click here or drop a file to upload!"}
+        {isDragActive && !isDragReject && "Drop it like it's hot!"}
+        {isDragReject && "File type not accepted, sorry!"}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UploadButton;
