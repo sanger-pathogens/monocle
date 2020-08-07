ddd04a21278999e2f9df57f94ef2964403a083ca
Gareth Peat <gp16@sanger.ac.uk>
2020-08-07 14:39:31 +0100

    Get e2e tests passing

--- ui/src/components/SampleDownloadButton.js
+++ ui/src/components/SampleDownloadButton.js
@@ -30,6 +30,16 @@ const handlerGenerator = (laneId) => {
     },
   ];
   const handler = () => {
+    // TODO: The alert was put in as a work around for cypress tests. Using filesaver creates a pop up asking where to save the file.
+    // This can not be blocked for the headless browser that we are using to run tests, may change in the future.
+    // If download testing becomes extensive may change to non-headless browser, then will need to remove the if statement here.
+    // Main discussion: https://github.com/cypress-io/cypress/issues/949
+    // Instructions on disabling pop ups: https://docs.cypress.io/api/plugins/browser-launch-api.html#Change-download-directory
+    if (window.Cypress) {
+      alert(downloads.map((d) => d.filename).join(";"));
+      return;
+    }
+
     const fileStream = streamSaver.createWriteStream(archiveName);
     const readableZipStream = new ZIP({
       start(ctrl) {},
