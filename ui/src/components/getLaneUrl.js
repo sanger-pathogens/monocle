194bd3961d03caea8c906b6b315f960d6a1b4de7
Gareth Peat <gp16@sanger.ac.uk>
2020-08-17 09:53:54 +0100

    Comment out unused code

--- ui/src/components/getLaneUrl.js
+++ ui/src/components/getLaneUrl.js
@@ -1,9 +1,9 @@
-import env from "../env";
+// import env from "../env";
 
-const getLaneUrl = (laneId) => {
-  let laneId_encoded = `${encodeURIComponent(`${laneId}`)}`;
-  let lane_url = `${env.DOWNLOAD_ROOT_URL}${laneId_encoded}`;
-  return lane_url;
-};
+// const getLaneUrl = (laneId) => {
+//   let laneId_encoded = `${encodeURIComponent(`${laneId}`)}`;
+//   let lane_url = `${env.DOWNLOAD_ROOT_URL}${laneId_encoded}`;
+//   return lane_url;
+// };
 
-export default getLaneUrl;
+// export default getLaneUrl;
