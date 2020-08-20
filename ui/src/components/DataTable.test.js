3f3249a99fb84fa470fc0561bd4268c3c60882c3
Gareth Peat <gp16@sanger.ac.uk>
2020-08-20 16:45:46 +0100

    Add pagination test first page

--- ui/src/components/DataTable.test.js
+++ ui/src/components/DataTable.test.js
@@ -24,34 +24,6 @@ describe("DataTable.pagination", () => {
     });
   });
 
-  it("can visit previous page", async () => {
-    const { getByLabelText, getByText } = render(
-      <DataTable
-        tableId="testTable"
-        columns={columns}
-        data={data}
-        totalCount={totalCount}
-        fetchData={fetchData}
-        pageCount={pageCount}
-        pageSize={pageSize}
-      />
-    );
-
-    expect(getByLabelText("next page")).toBeInTheDocument();
-    expect(getByText("1-10 of 40")).toBeInTheDocument();
-    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
-
-    fireEvent.click(getByLabelText("next page"));
-
-    expect(getByText("11-20 of 40")).toBeInTheDocument();
-    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 1 });
-
-    fireEvent.click(getByLabelText("previous page"));
-
-    expect(getByText("1-10 of 40")).toBeInTheDocument();
-    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
-  });
-
   it("can visit next page", async () => {
     const { getByLabelText, getByText } = render(
       <DataTable
@@ -96,5 +68,61 @@ describe("DataTable.pagination", () => {
 
     expect(getByText("31-40 of 40")).toBeInTheDocument();
     expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 3 });
+  });
+
+  it("can visit previous page", async () => {
+    const { getByLabelText, getByText } = render(
+      <DataTable
+        tableId="testTable"
+        columns={columns}
+        data={data}
+        totalCount={totalCount}
+        fetchData={fetchData}
+        pageCount={pageCount}
+        pageSize={pageSize}
+      />
+    );
+
+    expect(getByLabelText("next page")).toBeInTheDocument();
+    expect(getByText("1-10 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
+
+    fireEvent.click(getByLabelText("next page"));
+
+    expect(getByText("11-20 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 1 });
+
+    fireEvent.click(getByLabelText("previous page"));
+
+    expect(getByText("1-10 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
+  });
+
+  it("can visit first page", async () => {
+    const { getByLabelText, getByText } = render(
+      <DataTable
+        tableId="testTable"
+        columns={columns}
+        data={data}
+        totalCount={totalCount}
+        fetchData={fetchData}
+        pageCount={pageCount}
+        pageSize={pageSize}
+      />
+    );
+
+    expect(getByLabelText("last page")).toBeInTheDocument();
+    expect(getByText("1-10 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
+
+    fireEvent.click(getByLabelText("last page"));
+
+    expect(getByText("31-40 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 3 });
+
+    fireEvent.click(getByLabelText("first page"));
+
+    expect(getByText("1-10 of 40")).toBeInTheDocument();
+    expect(fetchData).toHaveBeenCalledWith({ pageSize: 10, pageIndex: 0 });
   });
 });
