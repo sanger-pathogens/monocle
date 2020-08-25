79fa7f199d1d4d3dd88fa6ad0cfbc22f1a678710
Gareth Peat <gp16@sanger.ac.uk>
2020-08-25 16:49:49 +0100

    Remove unused code

--- e2e/integration/upload-button.spec.js
+++ e2e/integration/upload-button.spec.js
@@ -11,16 +11,6 @@ import "cypress-file-upload";
 // Test commit changes the database
 
 describe("upload button", () => {
-  // beforeEach(function () {
-  //   this.dropEvent = {
-  //     dataTransfer: {
-  //       files: [
-  //         { path: "/Users/km22/Documents/git_projects/monocle/e2e/mock_data" },
-  //       ],
-  //     },
-  //   };
-  // });
-
   it("shows update button above empty samples table as Sanger user", () => {
     loadDatabaseProfile(DB_PROFILES.EMPTY).then((db) => {
       // load and login
@@ -96,7 +86,6 @@ describe("upload button", () => {
             {
               fileContent,
               fileName,
-              // mimeType: "image/png",
               mimeType: "application/vnd.ms-excel",
             },
             { subjectType: "drag-n-drop" }
@@ -108,74 +97,4 @@ describe("upload button", () => {
       cy.contains("Added: 3").should("exist");
     });
   });
-
-  // it("dropzone exists", () => {
-  //   loadDatabaseProfile(DB_PROFILES.EMPTY).then((db) => {
-  //     // empty samples table?
-  //     expect(db.sample.length).to.equal(0);
-
-  //     // load and login
-  //     cy.visit("/");
-  //     const user = db.user[0];
-  //     login(user.email);
-
-  //     //
-  //     cy.get('button').contains('Update metadata from spreadsheet').click()
-  //     cy.visit("/upload");
-
-  //     // drag and drop does exist
-  //     cy.get("#uploadButton").should(
-  //       "contain",
-  //       `Click here or drop a file to upload!`
-  //     );
-  //   });
-  // });
-
-  // it("drag drop exists when database contains samples", () => {
-  //   loadDatabaseProfile(DB_PROFILES.SMALL).then((db) => {
-  //     // non-empty samples table?
-  //     expect(db.sample.length).to.be.greaterThan(0);
-
-  //     // load and login
-  //     cy.visit("/");
-  //     const user = db.user[0];
-  //     login(user.email);
-
-  //     // drag and drop does exists
-  //     cy.get("#uploadButton").should(
-  //       "contain",
-  //       `Click here or drop a file to upload!`
-  //     );
-  //   });
-  // });
-
-  // it("drag and drop button is not there for non-sanger user", () => {
-  //   loadDatabaseProfile(DB_PROFILES.SMALL).then((db) => {
-  //     cy.visit("/");
-
-  //     // credentials
-  //     const user = db.user[1];
-  //     const { email, first_name, last_name } = user;
-
-  //     // fill in credentials form and submit
-  //     login(email);
-
-  //     // drag and drop does not exist
-  //     cy.get("body")
-  //       .contains("Click here or drop a file to upload!")
-  //       .should("not.exist");
-  //   });
-  // });
-
-  // it.only("uploads file from filesystem", () => {
-  //   loadDatabaseProfile(DB_PROFILES.SMALL).then((db) => {
-  //     // load and login
-  //     cy.visit("/");
-  //     const user = db.user[0];
-  //     login(user.email);
-
-  //     const yourFixturePath = "test.xlsx";
-  //     cy.get("#uploadButton").attachFile(yourFixturePath);
-  //   });
-  // });
 });
