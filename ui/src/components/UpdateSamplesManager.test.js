45b8422930bb7401f4cf697402585ffb29d43d32
kathryn1995 <km22@sanger.ac.uk>
2020-09-04 15:01:44 +0100

    Removed un-used files

--- ui/src/components/UpdateSamplesManager.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render, fireEvent, act } from "@testing-library/react";
-
-// import { DownloadingContext } from "../downloading";
-import UpdateSamplesManager from "./UpdateSamplesManager";
-import { mockUser } from "../test-utils/apiMocks";
-import MockProviders from "../test-utils/MockProviders";
