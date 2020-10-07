719686ca213eddb002f279dd058fc3a4377294a6
Gareth Peat <gp16@sanger.ac.uk>
2020-10-07 14:15:15 +0100

    Add page for password change

---
+++ ui/src/components/PageChangePassword.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Page from "./Page";
+import Footer from "./Footer";
+import ChangePassword from "./ChangePassword";
+
+const useStyles = makeStyles({
+  container: {
+    flex: 1,
+    display: "flex",
+    maxWidth: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const PageChangePassword = () => {
+  const classes = useStyles();
+  return (
+    <Page header={null} footer={<Footer />}>
+      <Box className={classes.container}>
+        <ChangePassword />
+      </Box>
+    </Page>
+  );
+};
+
+export default PageChangePassword;
