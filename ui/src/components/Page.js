a4e23d611e73a6b59365be0c4d0197d141594d64
Gareth Peat <gp16@sanger.ac.uk>
2020-10-09 10:02:15 +0100

    Move centering logic PageContent component

--- ui/src/components/Page.js
+++ ui/src/components/Page.js
@@ -3,36 +3,25 @@ import { Box } from "@material-ui/core";
 
 import PageHeader from "./PageHeader";
 import PageFooter from "./PageFooter";
+import PageContent from "./PageContent";
 
-const Page = ({ hideHeader, hideFooter, centerContent, children }) => {
-  const centering = centerContent
-    ? {
-        display: "flex",
-        flex: 1,
-        maxWidth: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }
-    : {};
-  return (
-    <Box
-      margin={0}
-      padding={0}
-      paddingTop={hideHeader ? 0 : "48px"}
-      maxWidth="100%"
-      minHeight="100vh"
-      display="flex"
-      alignItems="stretch"
-      flexDirection="column"
-      bgcolor="primary.dark"
-    >
-      {hideHeader ? null : <PageHeader />}
-      <Box flexGrow={1} {...centering}>
-        {children}
-      </Box>
-      {hideFooter ? null : <PageFooter />}
-    </Box>
-  );
-};
+const Page = ({ hideHeader, hideFooter, centerContent, children }) => (
+  <Box
+    margin={0}
+    padding={0}
+    paddingTop={hideHeader ? 0 : "48px"}
+    maxWidth="100%"
+    minHeight="100vh"
+    display="flex"
+    alignItems="stretch"
+    flexDirection="column"
+    bgcolor="primary.dark"
+    color="text.primary"
+  >
+    {hideHeader ? null : <PageHeader />}
+    <PageContent centerContent={centerContent}>{children}</PageContent>
+    {hideFooter ? null : <PageFooter />}
+  </Box>
+);
 
 export default Page;
