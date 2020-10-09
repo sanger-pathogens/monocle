21518e735d22703dce1c97e1d324061e63bc31ee
Gareth Peat <gp16@sanger.ac.uk>
2020-10-09 09:45:51 +0100

    Switch Paper for Box

--- ui/src/components/Page.js
+++ ui/src/components/Page.js
@@ -1,31 +1,26 @@
 import React from "react";
-import { Paper, Box } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Box } from "@material-ui/core";
 
 import PageHeader from "./PageHeader";
 import PageFooter from "./PageFooter";
 
-const useStyles = makeStyles((theme) => ({
-  page: {
-    margin: 0,
-    padding: 0,
-    maxWidth: "100%",
-    minHeight: "100vh",
-    display: "flex",
-    alignItems: "stretch",
-    flexDirection: "column",
-    background: theme.palette.primary.dark,
-  },
-}));
-
 const Page = ({ hideHeader, hideFooter, children }) => {
-  const classes = useStyles();
   return (
-    <Paper className={classes.page}>
+    <Box
+      margin={0}
+      padding={0}
+      paddingTop={hideHeader ? 0 : "48px"}
+      maxWidth="100%"
+      minHeight="100vh"
+      display="flex"
+      alignItems="stretch"
+      flexDirection="column"
+      bgcolor="primary.dark"
+    >
       {hideHeader ? null : <PageHeader />}
       {children ? <Box flexGrow={1}>{children}</Box> : null}
       {hideFooter ? null : <PageFooter />}
-    </Paper>
+    </Box>
   );
 };
 
