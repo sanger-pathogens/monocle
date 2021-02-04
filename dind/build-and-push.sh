50436d182714c8d698ebaa680fce93ee091e2cb1
Tim Stickland <ts24@sanger.ac.uk>
2021-02-04 15:59:39 +0000

    bug fixes -- wrong base image for CI docker image tweak script to build each stage as separate image

--- dind/build-and-push.sh
+++ dind/build-and-push.sh
@@ -18,8 +18,8 @@ if [ true != "${docker_login_done}" ]; then
    docker login ${REGISTRY} || exit 1
 fi
 
-
-for IMAGE in gitlab-ci-docker gitlab-ci-dind
+# build an image from each stage in the Dockerfile, and name the image the same as the stage
+for IMAGE in $(grep '^\s*FROM.*\bAS\s' Dockerfile | sed -e 's/.*AS//')
 do
 
    remote_image="${REGISTRY}/${IMAGE}"
@@ -27,9 +27,10 @@ do
    # build image and tag with commit SHA
    this_build="${IMAGE}:${COMMIT}"
    echo "🐳 Building ${this_build} 🐳"
-   docker build --build-arg  BUILDKIT_INLINE_CACHE=1   \
-                --cache-from "${remote_image}:latest"  \
-                --tag        ${this_build} .
+   docker build --build-arg   BUILDKIT_INLINE_CACHE=1    \
+                --cache-from  "${remote_image}:latest"   \
+                --target      ${IMAGE}                   \
+                --tag         ${this_build} .
                 
    # push image tagged with commit SHA and with 'latest' tag
    for TAG in ${COMMIT} 'latest'
