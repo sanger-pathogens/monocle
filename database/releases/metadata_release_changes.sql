830fa7ab358b01fc3eae8d60a2438dcf4907a542
kpepper <kp11@sanger.ac.uk>
2021-05-26 10:57:40 +0100

    Added deletion of samples

--- database/releases/metadata_release_changes.sql
+++ database/releases/metadata_release_changes.sql
@@ -1,7 +1,12 @@
 -- Database changes required for the first metadata code release
 
+-- Remove the current samples, so we can reload
+delete from api_sample;
+
+-- Update Hong Kong country name
 update api_institution set country = 'China [Hong Kong]' where name = 'The Chinese University of Hong Kong';
 
+-- Add new institution
 insert into api_institution (name, country, latitude, longitude)
 values ('Laboratório Central do Estado do Paraná', 'Brazil', -25.42537, -49.25920);
 
