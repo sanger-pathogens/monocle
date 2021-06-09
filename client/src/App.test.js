feef3409a229f81481505148a3c06663ce831722
ChadoNihi <alexandr.nil.io@gmail.com>
2021-06-09 17:43:07 +0100

    Add main (ie App) component

---
+++ client/src/App.test.js
@@ -0,0 +1,27 @@
+import { fireEvent, render } from "@testing-library/svelte";
+import App from "./App.svelte";
+
+it("renders the metadata uploading component", () => {
+  const { getByText } = render(App);
+
+  expect(getByText("Select or drag and drop your Excel files with metadata:"))
+    .toBeDefined();
+  expect(getByText("Upload"))
+    .toBeDefined();
+});
+
+it("redirects to the dashboard page on the upload success event", async () => {
+  const redirectionSpy = jest.fn();
+  const injectedWindow = {
+    location: { assign: redirectionSpy }
+  };
+  const { container } = render(App, { injectedWindow });
+
+  expect(redirectionSpy).not.toHaveBeenCalled();
+
+  await fireEvent.submit(container.querySelector("form"));
+
+  expect(redirectionSpy).toHaveBeenCalledTimes(1);
+  expect(redirectionSpy).toHaveBeenCalledWith("/dashboard");
+});
+
