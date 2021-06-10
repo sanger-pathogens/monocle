489faab10eb64347ad088aebf6ab96dfda839563
ts24 <ts24@sanger.ac.uk>
2021-06-10 16:57:20 +0000

    addi class  DataSources.user_data.UserData to dashboard with methods for retrieving user information from LDAP service WIP: only trivial methods so far

---
+++ dash/DataSources/user_data.py
@@ -0,0 +1,159 @@
+import copy
+import ldap
+import logging
+import yaml
+
+
+class UserDataError(Exception):
+   """ exception when user data methods are called with invalid parameter(s) """
+   pass
+
+class UserData:
+   """
+   Methods for retrieving user data from the Monocle LDAP service.
+   """
+   
+   data_sources_config           = 'data_sources.yml'
+   data_source                   = 'monocle_ldap'
+   data_source_config_file_param = 'openldap_config'
+   required_config_params        = [   data_source_config_file_param,
+                                       'ldap_url',
+                                       'users_ou',
+                                       'groups_ou',
+                                       'username_field',
+                                       'uid_field',
+                                       'gid_field',
+                                       ]
+   required_openldap_params      = [   'MONOCLE_LDAP_BASE_DN',
+                                       'MONOCLE_LDAP_BIND_DN',
+                                       'MONOCLE_LDAP_BIND_PASSWORD',
+                                       ]
+   # list OpenLDAP params here if they are to be excluded from logging
+   openldap_secret_params        = [   'LDAP_ADMIN_PASSWORD',
+                                       'LDAP_CONFIG_PASSWORD',
+                                       'LDAP_READONLY_USER_PASSWORD',
+                                       'MONOCLE_LDAP_BIND_PASSWORD',
+                                       ]
+
+   def __init__(self, set_up=True):
+      """
+      Constructor.  Reads config from default config file by default;
+      pass set_up=False to prevent config being dong on instalation, and
+      subsequently you can call set_up() to configure with a config file of your choice
+      """
+      self._current_ldap_connection   = None
+      if set_up:
+         self.set_up(self.data_sources_config)
+         # for logging, copy the config and remoce secrets
+         logged_config = copy.deepcopy(self.config)
+         for this_secret in self.openldap_secret_params:
+            logged_config['openldap'][this_secret] = 'SECRET'
+         logging.info("read monocle LDAP config: {}".format(logged_config))
+
+   def set_up(self, config_file_name):
+      """
+      read config from named file
+      """
+      with open(config_file_name, 'r') as file:
+         data_sources = yaml.load(file, Loader=yaml.FullLoader)
+         self.config = data_sources[self.data_source]
+         for required_param in self.required_config_params:
+            if not required_param in self.config:
+               logging.error("data source config file {} does not provide the required paramter {}.{}".format(config_file_name,self.data_source,required_param))
+               raise KeyError
+         # OpenLDAP config is in a separate config file
+         self.read_openldap_config(self.config[self.data_source_config_file_param])
+
+   def read_openldap_config(self, config_file_name):
+      """
+      read OpenLDAP config from named file
+      the default file will have been read if set_up() was called, but it can be called separately
+      if you want to load db credentials from a different config file
+      """
+      logging.info("Reading OpenLDAP config from {}".format(config_file_name))
+      with open(config_file_name, 'r') as file:
+         openldap_config = yaml.load(file, Loader=yaml.FullLoader)
+         for required_param in self.required_openldap_params:
+            if not required_param in openldap_config:
+               logging.error("OpenLDAP config file {} does not provide the required paramter {}".format(config_file_name,required_param))
+               raise KeyError
+         self.config['openldap'] = openldap_config
+
+   def connection(self):
+      """
+      If already connected to LDAP server, returns the connection.
+      If not connected, initializes LDAP connection, using URL from config value `ldap_url`, and returns the connection
+      Stores instance of `ldap.ldapobject.SimpleLDAPObject` connection class as self.connection, and returns same.
+      """
+      if self._current_ldap_connection is None:
+         logging.info("Connecting to LDAP server {}".format(self.config['ldap_url']))
+         conn = ldap.initialize(self.config['ldap_url'])
+         assert (isinstance(conn, ldap.ldapobject.SimpleLDAPObject)), "ldap.initialize was expected to return and instance of ldap.ldapobject.SimpleLDAPObject, not {}".format(conn)
+         conn.simple_bind_s(self.config['openldap']['MONOCLE_LDAP_BIND_DN'], self.config['openldap']['MONOCLE_LDAP_BIND_PASSWORD'])
+         self._current_ldap_connection = conn
+      return self._current_ldap_connection
+   
+   def disconnect(self):
+      """
+      If currently connected to LDAP server, disconencts.
+      Does nothing if already disconnected.
+      """
+      if self._current_ldap_connection is not None:
+         self._current_ldap_connection = None
+
+   def username_search(self, username):
+      """
+      Wraps ldap_search() with a username search for the value passed
+      """
+      logging.info('searching for username {}'.format(username))
+      result_list = self.ldap_search(  self.config['users_ou'],
+                                       self.config['username_field'],
+                                       username
+                                       )
+      # believe there should be only one hit -- or usernames aren't unique :-/
+      if 1 < len(result_list):
+         UserDataError("The username {} matched multiple entries in {}.{}:  it should be unique".format(username,self.config['users_ou'],self.config['username_field']))
+      result = result_list[0]
+      # return value should be tuple with two elements
+      #   1 the user's DN
+      #   2 a dict with the fields
+      # we should always have a GID in the fields
+      if self.config['gid_field'] not in result[1]:
+         UserDataError("username {} search result doesn't seem to contain the required field {} (complete data = {})".format(username,self.config['gid_field'],result))
+      # TODO more validation to check user data are OK
+      logging.info("found user: {}".format(result))
+      return result
+
+   def gid_search(self, gid):
+      """
+      Wraps ldap_search() with a GID search for the value passed
+      """
+      logging.info('searching for GID {}'.format(gid))
+      result_list = self.ldap_search(  self.config['groups_ou'],
+                                       self.config['gid_field'],
+                                       gid
+                                       )
+      # should be only one hit -- or GID isn't unique :-/
+      if 1 < len(result_list):
+         UserDataError("The GID {} matched multiple entries in {}.{}:  it should be unique".format(gid,self.config['groups_ou'],self.config['gid_field']))
+      result = result_list[0]
+      # TODO more validation to check group data are OK
+      logging.info("found group: {}".format(result))
+      return result
+   
+   def ldap_search(self, org_unit, field, value):
+      """
+      Generic LDAP search method
+      Searches the specfied organizational unit and field, for given value, and returns whatever data is retrieved from LDAP
+      """
+      if value is not None and 0 < len(str(value)):
+         UserDataError("LDAP search string must not be None and must not be an empty string")
+      this_search = '{}={}'.format(field,value)
+      logging.info('LDAP search: {}'.format(this_search))
+      # TODO add more graceful error handling
+      result = self.connection().search_s(self.config['openldap']['MONOCLE_LDAP_BASE_DN'],
+                                          ldap.SCOPE_SUBTREE,
+                                          this_search
+                                          )
+      logging.info("LDAP search result: {}".format(result))
+      return result
--- dash/db_test.py
+++ dash/db_test.py
@@ -3,12 +3,13 @@ import pprint
 import DataSources.monocledb
 import DataSources.sequencing_status
 import DataSources.pipeline_status
+import DataSources.user_data
 
 logging.basicConfig(format='%(asctime)-15s %(levelname)s:  %(message)s', level='INFO')
 
-monocledb         = DataSources.monocledb.MonocleDB()
-pp                = pprint.PrettyPrinter(indent=3)
+pp = pprint.PrettyPrinter(indent=3)
 
+#monocledb = DataSources.monocledb.MonocleDB()
 #print("Tables:\n")
 #for table in monocledb.connection().execute('show tables'):
    #table_name = table[0]
@@ -17,26 +18,26 @@ pp                = pprint.PrettyPrinter(indent=3)
       #print("   {}".format(desc))
       
       
-print("\nInstitution names:\n")
-inst_list = monocledb.get_institution_names()
-pp.pprint( inst_list )
+#print("\nInstitution names:\n")
+#inst_list = monocledb.get_institution_names()
+#pp.pprint( inst_list )
 
-print("\nSamples:\n")
-samples = monocledb.get_samples()
-print("First 3 samples:")
-pp.pprint( samples[0:3] )
+#print("\nSamples:\n")
+#samples = monocledb.get_samples()
+#print("First 3 samples:")
+#pp.pprint( samples[0:3] )
 
 
-sequencing_status = DataSources.sequencing_status.SequencingStatus()
-print("\nSequencing status:\n")
-test_set = samples[0:3]
-sample_id_list = [ s['sample_id'] for s in test_set ]
-sequencing_status_data = sequencing_status.get_multiple_samples(sample_id_list)
-#pp.pprint( sequencing_status_data )
-for this_sample in test_set:
-   this_sample['lane_id'] = [ lane['id'] for lane in sequencing_status_data[this_sample['sample_id']]['lanes'] ]
-   this_sample['sequencing_status'] = sequencing_status_data[this_sample['sample_id']]
-pp.pprint( test_set )
+#sequencing_status = DataSources.sequencing_status.SequencingStatus()
+#print("\nSequencing status:\n")
+#test_set = samples[0:3]
+#sample_id_list = [ s['sample_id'] for s in test_set ]
+#sequencing_status_data = sequencing_status.get_multiple_samples(sample_id_list)
+##pp.pprint( sequencing_status_data )
+#for this_sample in test_set:
+   #this_sample['lane_id'] = [ lane['id'] for lane in sequencing_status_data[this_sample['sample_id']]['lanes'] ]
+   #this_sample['sequencing_status'] = sequencing_status_data[this_sample['sample_id']]
+#pp.pprint( test_set )
 
 
 #pipeline_status = DataSources.pipeline_status.PipelineStatus()
@@ -51,3 +52,15 @@ pp.pprint( test_set )
       #else:
          #logging.debug("  no lane id: skipping")
 #pp.pprint( test_set )
+
+user_data = DataSources.user_data.UserData()
+ldap_conn = user_data.connection()
+
+for user in ['ts24','ts24.2','ts24.3']:
+   user_stuff  = user_data.username_search(user)
+   pp.pprint( "{}".format(user) )
+   pp.pprint( "   user data: {}".format(user_stuff) )
+   gid = user_stuff[1]['gidNumber'][0].decode('UTF-8')
+   #pp.pprint( "   gid: {}".format(gid) )
+   group_stuff = user_data.gid_search(gid)
+   pp.pprint( "   group data: {}".format(group_stuff) )
