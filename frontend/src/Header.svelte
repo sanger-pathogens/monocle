37cb2d6abfbbd3c1e853657a2da310d050ef581f
ChadoNihi <alexandr.nil.io@gmail.com>
2021-06-16 12:05:52 +0100

    Prefix asset URLs w/ `/upload` for frontend

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -5,7 +5,7 @@
 <div class="page_header">
    <div class="header_logo_container">
       <a href="https://www.gbsgen.net/">
-         <img alt="Juno Project" src="/imgs/JunoLogo.svg" class="header_logo" title="Juno Project">
+         <img alt="Juno Project" src="/upload/imgs/JunoLogo.svg" class="header_logo" title="Juno Project">
       </a>
    </div>
    <div class="header_links_container">
