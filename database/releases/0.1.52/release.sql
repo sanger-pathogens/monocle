f1f5be05c43023478073000070a2f9944f9234cf
ts24 <ts24@sanger.ac.uk>
2021-07-08 13:12:49 +0000

    added institutions; lat/long from https://geohack.toolforge.org/geohack.php?pagename=Universidade_Federal_de_Minas_Gerais&params=19_52_8.72_S_43_57_58.98_W https://geohack.toolforge.org/geohack.php?pagename=Universidade_Estadual_de_Londrina&params=23_19_31.09_S_51_11_59.92_W_

--- database/releases/0.1.52/release.sql
+++ database/releases/0.1.52/release.sql
@@ -1,3 +1,13 @@
+
+
+-- Add new institutions
+insert into api_institution (name, country, latitude, longitude)
+values ('Universidade Federal de Minas Gerais', 'Brazil', -19.869089, -43.966383);
+
+insert into api_institution (name, country, latitude, longitude)
+values ('Universidade Estadual de Londrina', 'Brazil', -23.325303, -51.199978);
+
+
 --
 -- Update the database version
 --
