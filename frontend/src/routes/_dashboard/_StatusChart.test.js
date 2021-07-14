6b07446c1d3dc14cd80ba085293ed98e97873ca4
ChadoNihi <alexandr.nil.io@gmail.com>
2021-07-14 11:01:42 +0100

    Add unit tests to cover new prop for StatusChart

--- frontend/src/routes/_dashboard/_StatusChart.test.js
+++ frontend/src/routes/_dashboard/_StatusChart.test.js
@@ -4,6 +4,10 @@ import StatusChart from "./_StatusChart.svelte";
 
 jest.mock("frappe-charts");
 
+beforeEach(() => {
+  Chart.mockClear();
+});
+
 it("computes correct completion percentage for the title", () => {
   const pending = 66;
   const succeeded = 120;
@@ -21,5 +25,30 @@ it("passes the right colors to the chart library", () => {
   const chartOptions = Chart.mock.calls[0][1];
   expect(chartOptions.colors)
     .toEqual(["light-grey", "light-green", "red"]);
+});
+
+describe("includes running", () => {
+  it("computes correct completion percentage for the title", () => {
+    const pending = 66;
+    const running = 36;
+    const succeeded = 120;
+    const failed = 2;
+
+    render(StatusChart, {
+      values: [pending, running, succeeded, failed],
+      includesRunning: true
+    });
+
+    const chartOptions = Chart.mock.calls[0][1];
+    expect(chartOptions.title).toBe("54.5% completed");
+  });
+
+  it("passes the right colors to the chart library", () => {
+    render(StatusChart, { includesRunning: true });
+
+    const chartOptions = Chart.mock.calls[0][1];
+    expect(chartOptions.colors)
+      .toEqual(["light-grey", "light-blue", "light-green", "red"]);
+  });
 });
 
