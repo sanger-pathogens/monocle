1fa6bb0aec48a6d02f92edad82db4f34c64647b8
CharlesNunn001 <cn14@sanger.ac.uk>
2021-07-15 08:46:16 +0100

    routes testing

--- metadata/juno/metadata/tests/routes_test.py
+++ metadata/juno/metadata/tests/routes_test.py
@@ -19,26 +19,59 @@ class TestRoutes(unittest.TestCase):
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_samples')
     @patch('metadata.api.routes.convert_to_json')
     def test_get_samples_jsonified(self, mocked_jsoncall, mocked_query):
-        mocked_query.return_value = ''
-        mocked_jsoncall.return_value = ''
-        mocked_jsoncall.assert_called_once()
+        mocked_query.return_value = ['sample1', 'sample2']
+        mocked_jsoncall.return_value = 'expected'
+
         under_test = mar.get_samples()
-        self.assertEqual(under_test, '')
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['expected', 200])
+
+    @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_samples')
+    @patch('metadata.api.routes.convert_to_json')
+    def test_get_samples_no_return(self, mocked_jsoncall, mocked_query):
+        mocked_query.return_value = []
+        mocked_jsoncall.return_value = ''
+
+        under_test = mar.get_samples()
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['', 404])
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institutions')
     @patch('metadata.api.routes.convert_to_json')
-    def test_get_samples_jsonified(self, mocked_jsoncall, mocked_query):
-        mocked_query.return_value = ''
-        mocked_jsoncall.return_value = ''
-        mocked_jsoncall.assert_called_once()
+    def test_get_institutions_jsonified(self, mocked_jsoncall, mocked_query):
+        mocked_query.return_value = ['ints1', 'inst2']
+        mocked_jsoncall.return_value = 'expected'
+
         under_test = mar.get_samples()
-        self.assertEqual(under_test, '')
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['expected', 200])
+
+    @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institutions')
+    @patch('metadata.api.routes.convert_to_json')
+    def test_get_institutions_not_returned(self, mocked_jsoncall, mocked_query):
+        mocked_query.return_value = []
+        mocked_jsoncall.return_value = ''
+
+        under_test = mar.get_samples()
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['', 404])
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institution_names')
     @patch('metadata.api.routes.convert_to_json')
-    def test_get_samples_jsonified(self, mocked_jsoncall, mocked_query):
-        mocked_query.return_value = ''
-        mocked_jsoncall.return_value = ''
-        mocked_jsoncall.assert_called_once()
+    def test_get_institution_names_jsonified(self, mocked_jsoncall, mocked_query):
+        mocked_query.return_value = ['ints1', 'inst2']
+        mocked_jsoncall.return_value = 'expected'
+
         under_test = mar.get_samples()
-        self.assertEqual(under_test, '')
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['expected', 200])
+
+    @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institution_names')
+    @patch('metadata.api.routes.convert_to_json')
+    def test_get_institution_names_not_returned(self, mocked_jsoncall, mocked_query):
+        mocked_query.return_value = []
+        mocked_jsoncall.return_value = ''
+
+        under_test = mar.get_samples()
+        mocked_jsoncall.assert_called_once()
+        self.assertEqual(under_test, ['', 404])
