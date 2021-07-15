c837e35dd8dd4ecec6b35955f2b619c14cf66bfd
CharlesNunn001 <cn14@sanger.ac.uk>
2021-07-15 12:11:47 +0100

    Changed list to tuple for routes test

--- metadata/juno/metadata/tests/routes_test.py
+++ metadata/juno/metadata/tests/routes_test.py
@@ -26,7 +26,7 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_samples = MagicMock(return_value=['sample1', 'sample2'])
         under_test = mar.get_samples(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['expected', 200])
+        self.assertEqual(under_test, ('expected', 200))
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_samples')
     @patch('metadata.api.routes.convert_to_json')
@@ -37,7 +37,7 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_samples = MagicMock(return_value=[''])
         under_test = mar.get_samples(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['', 404])
+        self.assertEqual(under_test, ('', 404))
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institutions')
     @patch('metadata.api.routes.convert_to_json')
@@ -48,7 +48,7 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_samples = MagicMock(return_value=['inst1', 'inst2'])
         under_test = mar.get_institutions(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['expected', 200])
+        self.assertEqual(under_test, ('expected', 200))
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institutions')
     @patch('metadata.api.routes.convert_to_json')
@@ -59,7 +59,7 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_samples = MagicMock(return_value=[''])
         under_test = mar.get_institutions(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['', 404])
+        self.assertEqual(under_test, ('', 404))
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institution_names')
     @patch('metadata.api.routes.convert_to_json')
@@ -70,7 +70,7 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_samples = MagicMock(return_value=['ints1', 'inst2'])
         under_test = mar.get_institution_names(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['expected', 200])
+        self.assertEqual(under_test, ('expected', 200))
 
     @patch('metadata.api.database.monocle_database_service.MonocleDatabaseService.get_institution_names')
     @patch('metadata.api.routes.convert_to_json')
@@ -81,4 +81,4 @@ class TestRoutes(unittest.TestCase):
         fakeDB.get_institutions = MagicMock(return_value=[''])
         under_test = mar.get_institution_names(fakeDB)
         mocked_jsoncall.assert_called_once()
-        self.assertEqual(under_test, ['', 404])
+        self.assertEqual(under_test, ('', 404))
