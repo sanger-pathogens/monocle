5105c74168e63fe8318855c9b5873674b258da53
kpepper <kp11@sanger.ac.uk>
2021-07-16 09:22:12 +0100

    Changed http error status to forbidden

--- dash-api/juno/dash/api/error_handlers.py
+++ dash-api/juno/dash/api/error_handlers.py
@@ -1,13 +1,12 @@
 """ Module for all error handler methods """
-import json
 from flask import Response
 
 
 def handle_unauthorised(exception):
     """ Handle an unauthorised exception """
-    return Response(response=json.dumps({'error': 'User authorisation error'}), status=401, mimetype="application/json")
+    return Response(response='User authorisation error', status=403, mimetype="text/plain")
 
 
 def handle_user_data_error(exception):
     """ Handle an user data error """
-    return Response(response=json.dumps({'error': 'User authorisation error'}), status=401, mimetype="application/json")
+    return Response(response='User authorisation error', status=403, mimetype="text/plain")
