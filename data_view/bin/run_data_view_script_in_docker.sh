2f65e547ffa2c269b59bf89d44c25878660ae05c
kpepper <kp11@sanger.ac.uk>
2021-07-23 10:53:34 +0100

    Added in md5 file creation and changed docker run user

--- data_view/bin/run_data_view_script_in_docker.sh
+++ data_view/bin/run_data_view_script_in_docker.sh
@@ -17,10 +17,25 @@
 
 SAMPLE_DATA_PATH="${HOME}/monocle_juno"
 
-docker run  --volume ${SAMPLE_DATA_PATH}:${SAMPLE_DATA_PATH}  \
+# Using the -u option ensures that file/directories are owned by the application user rather than root
+docker run  -u `id -u`:`id -g`
+            --volume ${SAMPLE_DATA_PATH}:${SAMPLE_DATA_PATH}  \
             --volume `pwd`/monocle_juno_institution_view:/dash/monocle_juno_institution_view  \
             --volume `pwd`/my.cnf:/dash/my.cnf \
             --volume `pwd`/mlwh-api.yml:/dash/mlwh-api.yml \
             --volume `pwd`/create_download_view_for_sample_data.py:/dash/create_download_view_for_sample_data.py \
             gitlab-registry.internal.sanger.ac.uk/sanger-pathogens/monocle/monocle-dash:<DOCKERTAG> \
             python3 ./create_download_view_for_sample_data.py --data_dir "$SAMPLE_DATA_PATH" $@ 
+
+# Add md5 checksum files to each lane after all the directories have been created.
+# Doing this here should ensure that any new lanes/data get md5 files added asap.
+# If this step proves too slow, we may need to consider moving it to separate cron entry
+# or changing the job frequency.
+for DIR in $(find ${HOME}/monocle_juno_institution_view -mindepth 2 -maxdepth 2 -type d)
+do
+    cd "${DIR}"
+    if ls * > /dev/null 2>&1
+    then
+        md5sum $(ls * | grep -v '\.md5$') >$(basename "${DIR}").md5
+    fi
+done
