c0fc757a49cb44865eabc844d989adf7edca1564
ChadoNihi <alexandr.nil.io@gmail.com>
2021-08-10 16:35:47 +0100

    Add basic login page

---
+++ frontend/src/routes/login/index.svelte
@@ -0,0 +1,36 @@
+<h2>Log in</h2>
+
+<form>
+  <label>
+    Username
+    <input required />
+  </label>
+
+  <label>
+    Password
+    <input type="password" required />
+  </label>
+
+  <button type="submit">
+    Log in
+  </button>
+</form>
+
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  width: 14rem;
+  max-width: 78vw;
+}
+
+button {
+  margin-top: 1rem;
+}
+</style>
+
