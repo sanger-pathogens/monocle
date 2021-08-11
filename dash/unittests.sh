276cbdd159a8d1a5542dee36b5e3d01e2ad66f54
ts24 <ts24@sanger.ac.uk>
2021-08-11 11:53:28 +0000

    enabled python coverage for dash and metadata, so I can check coverage in this branch

--- dash/unittests.sh
+++ dash/unittests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHONPATH='.'
-python3 -m unittest discover -v -s tests -p '*_test.py'
+coverage run --source DataSources,MonocleDash --omit MonocleDash/components.py -m unittest discover -v -s tests -p '*_test.py'
