597e1b45ff3b61835676add78b0e982f4949475c
ts24 <ts24@sanger.ac.uk>
2021-08-11 12:10:03 +0000

    Revert "enabled python coverage for dash and metadata, so I can check"
    This reverts commit 276cbdd159a8d1a5542dee36b5e3d01e2ad66f54.

--- dash/unittests.sh
+++ dash/unittests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHONPATH='.'
-coverage run --source DataSources,MonocleDash --omit MonocleDash/components.py -m unittest discover -v -s tests -p '*_test.py'
+python3 -m unittest discover -v -s tests -p '*_test.py'
--- metadata/juno/unittests.sh
+++ metadata/juno/unittests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHONPATH='.'
-coverage run --source '.' --omit 'metadata/tests/*' -m unittest discover -v -s metadata/tests -p '*_test.py'
+python3 -m unittest discover -v -s metadata/tests -p '*_test.py'
