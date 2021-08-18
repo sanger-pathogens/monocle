1c8e2a6033e58ef72226311a717ca2accf7c5354
ts24 <ts24@sanger.ac.uk>
2021-08-18 10:32:14 +0000

    fix python path for unittest for institution-based data view script

--- data_view/unittests.sh
+++ data_view/unittests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=$(pwd)'/../dash'
+export PYTHONPATH="$(pwd)/../dash-api/juno:$(pwd)/../dash-api/juno/dash/api/service"
 
 coverage run --source . -m unittest discover -v -s ./tests -p '*_test.py'
