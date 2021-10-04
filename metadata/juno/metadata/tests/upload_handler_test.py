ec9be14e166b0ad2dbd132a8e775f169b5454267
Victoria Carr <vc11@sanger.ac.uk>
2021-10-04 16:29:28 +0100

    More informative validation error messages, remove validation for cps_type, ST and allelic frequencies

--- metadata/juno/metadata/tests/upload_handler_test.py
+++ metadata/juno/metadata/tests/upload_handler_test.py
@@ -413,100 +413,82 @@ class TestInSilicoUploadHandler(unittest.TestCase):
 
     def __check_validation_errors(self, validation_errors: List[str]):
         """ Assert validation errors are correct """
-        self.assertEqual(len(validation_errors), 46)
+        self.assertEqual(len(validation_errors), 38)
 
         self.assertTrue(
             '{row: 2, column: "Sample_id"}: "ZZZ;;{}{}{[[STUDY" contains illegal characters' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "cps_type"}: "@" contains illegal characters' in validation_errors)
+            '{row: 2, column: "23S1"}: "[pos]" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "ST"}: "ST=I" contains illegal characters' in validation_errors)
+            '{row: 3, column: "23S3"}: "*" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "adhP"}: "abc" must be a whole positive number and/or asterisk' in validation_errors)
-        #self.assertTrue(
-        #    '{row: 2, column: "pheS"}: "*" must be a whole positive number' in validation_errors)
+            '{row: 3, column: "ERMB"}: "_" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "atr"}: "0!" must be a whole positive number and/or asterisk' in validation_errors)
+            '{row: 2, column: "ERMT"}: "0" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "glnA"}: "1_6" must be a whole positive number and/or asterisk' in validation_errors)
+            '{row: 3, column: "FOSA"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "sdhA"}: "2.2" must be a whole positive number and/or asterisk' in validation_errors)
+            '{row: 2, column: "GYRA"}: "+" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "glcK"}: "." must be a whole positive number and/or asterisk' in validation_errors)
+            '{row: 3, column: "LNUB"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "tkt"}: "abc" must be a whole positive number and/or asterisk' in validation_errors)
+            '{row: 2, column: "LSAC"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "23S1"}: "[pos]" must be pos and neg' in validation_errors)
+            '{row: 3, column: "MEFA"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "23S3"}: "*" must be pos and neg' in validation_errors)
+            '{row: 2, column: "MPHC"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "ERMB"}: "_" must be pos and neg' in validation_errors)
+            '{row: 3, column: "MSRA"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "ERMT"}: "0" must be pos and neg' in validation_errors)
+            '{row: 2, column: "MSRD"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "FOSA"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "PARC"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "GYRA"}: "+" must be pos and neg' in validation_errors)
+            '{row: 2, column: "RPOBGBS-1"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "LNUB"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "RPOBGBS-2"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "LSAC"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "RPOBGBS-3"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "MEFA"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "RPOBGBS-4"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "MPHC"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "SUL2"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "MSRA"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "TETB"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "MSRD"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "TETL"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "PARC"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "TETM"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "RPOBGBS-1"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "TETO"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "RPOBGBS-2"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "TETS"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "RPOBGBS-3"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "ALP1"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "RPOBGBS-4"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "ALP23"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "SUL2"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "ALPHA"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "TETB"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "HVGA"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "TETL"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "PI1"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "TETM"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "PI2A1"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "TETO"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "PI2A2"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "TETS"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "PI2B"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "ALP1"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "RIB"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "ALP23"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "SRR1"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "ALPHA"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 2, column: "SRR2"}: "\'\'" must be pos, neg or empty' in validation_errors)
         self.assertTrue(
-            '{row: 3, column: "HVGA"}: "\'\'" must be pos and neg' in validation_errors)
+            '{row: 3, column: "23S1_variant"}: "\'\'" must contain comma-separated variants (e.g. T78Q,L55A), * for the common variant or empty' in validation_errors)
         self.assertTrue(
-            '{row: 2, column: "PI1"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 3, column: "PI2A1"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 2, column: "PI2A2"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 3, column: "PI2B"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 2, column: "RIB"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 3, column: "SRR1"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 2, column: "SRR2"}: "\'\'" must be pos and neg' in validation_errors)
-        self.assertTrue(
-            '{row: 3, column: "23S1_variant"}: "\'\'" contains illegal characters' in validation_errors)
-        self.assertTrue(
-            '{row: 3, column: "23S3_variant"}: "\'\'" contains illegal characters' in validation_errors)
+            '{row: 3, column: "23S3_variant"}: "\'\'" must contain comma-separated variants (e.g. T78Q,L55A), * for the common variant or empty' in validation_errors)
 
     def test_allowed_file_types(self):
         self.assertIsNotNone(self.under_test.allowed_file_types())
