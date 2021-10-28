2469f5d661df0eb68295422f40b02c924c71e614
Victoria Carr <vc11@sanger.ac.uk>
2021-10-28 11:59:54 +0100

    Remove redundant test

--- frontend/src/routes/_dashboard/_StatusChart.test.js
+++ frontend/src/routes/_dashboard/_StatusChart.test.js
@@ -26,26 +26,3 @@ it("passes the right colors to the chart library", () => {
   expect(chartOptions.colors)
     .toEqual(["light-grey", "light-green", "red"]);
 });
-
-describe("includes pending", () => {
-  it("computes correct completion percentage for the title", () => {
-    const pending = 66;
-    const succeeded = 120;
-    const failed = 2;
-
-    render(StatusChart, {
-      values: [pending, succeeded, failed]
-    });
-
-    const chartOptions = Chart.mock.calls[0][1];
-    expect(chartOptions.title).toBe("64.9% completed");
-  });
-
-  it("passes the right colors to the chart library", () => {
-    render(StatusChart);
-
-    const chartOptions = Chart.mock.calls[0][1];
-    expect(chartOptions.colors)
-      .toEqual(["light-grey", "light-green", "red"]);
-  });
-});
