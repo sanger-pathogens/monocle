080011d15a704e1d2152614596679255437e7c08
ts24 <ts24@sanger.ac.uk>
2021-11-08 13:07:10 +0000

    dash-api: syntax error fix

--- dash-api/juno/dash/api/service/data_services.py
+++ dash-api/juno/dash/api/service/data_services.py
@@ -241,7 +241,7 @@ class MonocleData:
                sequencing_status[this_institution] = self.sequencing_status_source.get_multiple_samples(sample_id_list)
             except urllib.error.HTTPError:
                logging.warning("{}.get_sequencing_status() failed to collect {} samples for unknown reason".format(__class__.__name__,len(sample_id_list)))
-               logging.info("{}.get_sequencing_status() failed to collect these {} samples: {}".format(__class__.__name__,len(sample_id_list,sample_id_list))
+               logging.info("{}.get_sequencing_status() failed to collect these {} samples: {}".format(__class__.__name__,len(sample_id_list),sample_id_list))
                sequencing_status[this_institution][self.api_key_error] = 'Server Error: Records cannot be collected at this time. Please try again later.'
          if '_ERROR' not in sequencing_status[this_institution].keys():
             sequencing_status[this_institution][self.api_key_error] = None
