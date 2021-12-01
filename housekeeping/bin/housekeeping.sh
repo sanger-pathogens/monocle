de56f5bc9242b15d7b0b029ca1e326f615ec0794
ts24 <ts24@sanger.ac.uk>
2021-12-01 18:21:41 +0000

    tweak housekeeping script so that it works with mode 331 bulk download directory

--- housekeeping/bin/housekeeping.sh
+++ housekeeping/bin/housekeeping.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
 SERVICE_INSTALL_DIR="$HOME"
+UNREADBLE_DIR_MODE="331" # for directories that must be unreadable so randomly-named links/files cannot be browsed
 
 BULK_DOWNLOAD_DIR="${SERVICE_INSTALL_DIR}/monocle_juno_institution_view/downloads/"
+TEMP_BULK_DOWNLOAD_DIR="${SERVICE_INSTALL_DIR}/temp_bulk_downloads/"
 BULK_DOWNLOAD_GLOB="*.zip"
-BULK_DOWNLOAD_EXPIRY="14"  # age in days when matching files should be deleted
+BULK_DOWNLOAD_EXPIRY="14"     # age in days when matching files should be deleted
 
 WEB_DOWNLOAD_LINK_DIR="${SERVICE_INSTALL_DIR}/monocle_juno_web_root/downloads/"
 TEMP_DOWNLOAD_LINK_DIR="${SERVICE_INSTALL_DIR}/temp_web_downloads/"
-WEB_DOWNLOAD_LINK_EXPIRY="30"     # age in days when links should be deleted
-WEB_DOWNLOAD_LINK_DIR_MODE="331"  # directory must be unreadable so the randomly-named links cannot be browsed
+WEB_DOWNLOAD_LINK_EXPIRY="30" # age in days when links should be deleted
+
 
 # Delete expired bulk download files
-find "$BULK_DOWNLOAD_DIR" -name "$BULK_DOWNLOAD_GLOB" -follow -mtime +"$BULK_DOWNLOAD_EXPIRY" -exec rm {} \;
+# Note the bulk downloads directory is moved (to a location inaccessible via the web) and made readable to do the cleanup.
+# Moving it back is conditional on the chmod restoring the correct mode for the web-accessible location.
+mv "$BULK_DOWNLOAD_DIR" "$TEMP_BULK_DOWNLOAD_DIR"
+chmod 700 "$TEMP_BULK_DOWNLOAD_DIR"
+find "$TEMP_BULK_DOWNLOAD_DIR" -name "$BULK_DOWNLOAD_GLOB" -follow -mtime +"$BULK_DOWNLOAD_EXPIRY" -exec rm {} \;
+chmod "$UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR" && mv "$TEMP_BULK_DOWNLOAD_DIR" "$BULK_DOWNLOAD_DIR"
+
 
 # Delete expired web download symlinks.
-# The web download directory must be unreadable at all times, so that downlaod are 
-# only possible with prior knowledge of the random link name  (i.e. users must not 
-# be able to see other link names in their browser). => we need to move the 
-# directory temporarily and make it readable before doing the clean up.  We 
-# **MUST** restore the correct mode before moving it back to the proper location! 
-# This will make download impossible for a second or two whilst this job runs.
+# Note the download symlinks directory is moved (to a location inaccessible via the web) and made readable to do the cleanup.
+# Moving it back is conditional on the chmod restoring the correct mode for the web-accessible location.
 mv "$WEB_DOWNLOAD_LINK_DIR" "$TEMP_DOWNLOAD_LINK_DIR"
 chmod 700 "$TEMP_DOWNLOAD_LINK_DIR"
 find "$TEMP_DOWNLOAD_LINK_DIR" -type l -mtime +"$WEB_DOWNLOAD_LINK_EXPIRY" -exec rm {} \;
-chmod "$WEB_DOWNLOAD_LINK_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR"
-mv "$TEMP_DOWNLOAD_LINK_DIR" "$WEB_DOWNLOAD_LINK_DIR"
+chmod "$UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR" && mv "$TEMP_DOWNLOAD_LINK_DIR" "$WEB_DOWNLOAD_LINK_DIR"
