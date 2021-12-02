6be605dc723f7955512b21795cae0052bb07c2ce
ts24 <ts24@sanger.ac.uk>
2021-12-02 16:44:54 +0000

    Housekeeping script: Previous version moved download dirs whilst they were made readable for mainetance, but they remained accessible (volumn mount used inodes, not dir names). Instead, temporarily stop access to service whilst maintenaince runs. This is done by switching NGINX conf files and a container restart, so it's really fast.

--- housekeeping/bin/housekeeping.sh
+++ housekeeping/bin/housekeeping.sh
@@ -1,31 +1,43 @@
 #!/usr/bin/env bash
 
+
+
 SERVICE_INSTALL_DIR="$HOME"
-UNREADBLE_DIR_MODE="331" # for directories that must be unreadable so randomly-named links/files cannot be browsed
+# some directories that must be unreadable, so randomly-named links/files cannot be browsed
+# for the housekeeping these must be made readable temporarily, then the usual mode restored
+# these are the modes:
+TEMP_READBLE_DIR_MODE="700"
+NORMAL_UNREADBLE_DIR_MODE="331"
+
+NGINX_CONTAINER="proxy"
+NGINX_SERVICE_CONF="nginx.proxy.conf"
+NGINX_SERVICE_CONF_TEMP_MOVE="${NGINX_SERVICE_CONF}.TEMP_MOVE"
+NGINX_MAINTENACE_CONF="nginx.service_maintenance.conf" # returns 503 to all requests
 
 BULK_DOWNLOAD_DIR="${SERVICE_INSTALL_DIR}/monocle_juno_institution_view/downloads/"
-TEMP_BULK_DOWNLOAD_DIR="${SERVICE_INSTALL_DIR}/temp_bulk_downloads/"
 BULK_DOWNLOAD_GLOB="*.zip"
 BULK_DOWNLOAD_EXPIRY="14"     # age in days when matching files should be deleted
 
 WEB_DOWNLOAD_LINK_DIR="${SERVICE_INSTALL_DIR}/monocle_juno_web_root/downloads/"
-TEMP_DOWNLOAD_LINK_DIR="${SERVICE_INSTALL_DIR}/temp_web_downloads/"
 WEB_DOWNLOAD_LINK_EXPIRY="30" # age in days when links should be deleted
 
 
-# Delete expired bulk download files
-# Note the bulk downloads directory is moved (to a location inaccessible via the web) and made readable to do the cleanup.
-# Moving it back is conditional on the chmod restoring the correct mode for the web-accessible location.
-mv "$BULK_DOWNLOAD_DIR" "$TEMP_BULK_DOWNLOAD_DIR"
-chmod 700 "$TEMP_BULK_DOWNLOAD_DIR"
-find "$TEMP_BULK_DOWNLOAD_DIR" -name "$BULK_DOWNLOAD_GLOB" -follow -mtime +"$BULK_DOWNLOAD_EXPIRY" -exec rm {} \;
-chmod "$UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR" && mv "$TEMP_BULK_DOWNLOAD_DIR" "$BULK_DOWNLOAD_DIR"
 
+# disable access to service at proxy
+mv "$NGINX_SERVICE_CONF" "$NGINX_SERVICE_CONF_TEMP_MOVE"
+cp "$NGINX_MAINTENACE_CONF" "$NGINX_SERVICE_CONF"
+docker-compose restart "$NGINX_CONTAINER"
+
+# Delete expired bulk download files
+chmod "$TEMP_READBLE_DIR_MODE" "$TEMP_BULK_DOWNLOAD_DIR"
+find "$TEMP_BULK_DOWNLOAD_DIR" -name "$BULK_DOWNLOAD_GLOB" -follow -mtime +"$BULK_DOWNLOAD_EXPIRY" -exec rm {} \;
+chmod "$NORMAL_UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR"
 
 # Delete expired web download symlinks.
-# Note the download symlinks directory is moved (to a location inaccessible via the web) and made readable to do the cleanup.
-# Moving it back is conditional on the chmod restoring the correct mode for the web-accessible location.
-mv "$WEB_DOWNLOAD_LINK_DIR" "$TEMP_DOWNLOAD_LINK_DIR"
-chmod 700 "$TEMP_DOWNLOAD_LINK_DIR"
+chmod "$TEMP_READBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR"
 find "$TEMP_DOWNLOAD_LINK_DIR" -type l -mtime +"$WEB_DOWNLOAD_LINK_EXPIRY" -exec rm {} \;
-chmod "$UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR" && mv "$TEMP_DOWNLOAD_LINK_DIR" "$WEB_DOWNLOAD_LINK_DIR"
+chmod "$NORMAL_UNREADBLE_DIR_MODE" "$TEMP_DOWNLOAD_LINK_DIR"
+
+# reenable access to service at proxy
+mv "$NGINX_SERVICE_CONF_TEMP_MOVE" "$NGINX_SERVICE_CONF"
+docker-compose restart "$NGINX_CONTAINER"
