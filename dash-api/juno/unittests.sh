5d5bc6a9c290926152497979c08cf7d0b84eee1f
ts24 <ts24@sanger.ac.uk>
2021-12-14 15:30:14 +0000

    Not useful to include test coverage of Connexion application.py file. Is useful to show missing liens in the report.

--- dash-api/juno/unittests.sh
+++ dash-api/juno/unittests.sh
@@ -2,4 +2,4 @@
 
 # TODO This needs tidying up (along with package structure) - for the moment the dash backend modules/tests are hacked in while changes are still ongoing
 export PYTHONPATH=".:./dash/api:./dash/api/service"
-coverage run --source '.' --omit 'dash/tests/*,*/service_factory.py' -m unittest discover -v -s . -p '*_test.py'
+coverage run --source '.' --omit 'dash/tests/*,*/service_factory.py,*/application.py' -m unittest discover -v -s . -p '*_test.py'
