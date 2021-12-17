eee1f8d16328bb7bfe96fcc039d33d20bfaff449
ChadoNihi <alexandr.nil.io@gmail.com>
2021-12-17 16:42:48 +0000

    FE bulk download: Extract token from URL before ...
    ... the component is rendered to prevent a split-second error screen.

--- frontend/src/routes/samples/download/[token].svelte
+++ frontend/src/routes/samples/download/[token].svelte
@@ -1,7 +1,15 @@
+<script context="module">
+  // Extract `token` from the URL path and pass it to the page component as a prop.
+  export function load({ page }) {
+    return { props: { downloadToken: page.params.token } };
+  }
+</script>
+
 <script>
   import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
 
-  const downloadToken = window.location.pathname.split("/").pop();
+  export let downloadToken;
+
   const downloadUrl = `/data_download/${downloadToken}`
   let downloadPrepared = false;
 
@@ -19,7 +27,7 @@
 
 {#if downloadPrepared}
   <p>
-    Your download is ready. (If you don't see a prompt to save the file, follow this <a href={downloadUrl} download>link</a>.)
+    Your download is ready. You can close this tab once the download starts. (If you don't see a prompt to save the file, follow this <a href={downloadUrl} download>download link</a>.)
   </p>
 
 {:else}
