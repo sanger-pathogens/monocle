492a8b6d59521b443147e2e030f8d5dabfd25857
ChadoNihi <alexandr.nil.io@gmail.com>
2021-12-17 16:23:54 +0000

    FE bulk download: Display message when download is ready

--- frontend/src/routes/samples/download/[token].svelte
+++ frontend/src/routes/samples/download/[token].svelte
@@ -3,6 +3,11 @@
 
   const downloadToken = window.location.pathname.split("/").pop();
   const downloadUrl = `/data_download/${downloadToken}`
+  let downloadPrepared = false;
+
+  function beforeDownload() {
+    downloadPrepared = true;
+  }
 </script>
 
 
@@ -10,6 +15,16 @@
   <meta http-equiv="refresh" content="0; url={downloadUrl}" />
 </svelte:head>
 
-<LoadingIndicator
-  message="Please wait: your download is being prepared. Large downloads may take a minute or two."
-/>
+<svelte:window on:beforeunload={beforeDownload} />
+
+{#if downloadPrepared}
+  <p>
+    Your download is ready. (If you don't see a prompt to save the file, follow this <a href={downloadUrl} download>link</a>.)
+  </p>
+
+{:else}
+  <LoadingIndicator
+    message="Please wait: your download is being prepared. Large downloads may take a minute or two."
+  />
+
+{/if}
