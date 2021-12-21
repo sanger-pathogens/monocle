217f45a2fa39ebc01a97ef88ff0599e076ee5ae5
ChadoNihi <alexandr.nil.io@gmail.com>
2021-12-21 14:51:35 +0000

    FE download interstitial: Replace fetch w/ XMLHttpRequest ...
    ... to avoid browser-specific timeouts

--- frontend/src/routes/samples/download/[token].svelte
+++ frontend/src/routes/samples/download/[token].svelte
@@ -6,41 +6,56 @@
 </script>
 
 <script>
-  import { onMount } from "svelte";
   import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
 
   export let downloadToken;
 
   const downloadUrl = `/data_download/${downloadToken}`
-  let downloadPrepared = false;
 
-  onMount(() => {
-    // FIXME use `XMLHttpRequest` instead to avoid `fetch`'s browser-specific timeout?
-    fetch(downloadUrl, { redirect: "follow" })
-      .then((response) => {
-        downloadPrepared = true;
-        window.location.assign(response.url);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const prepareDownloadPromise = new Promise((resolve, reject) => {
+    // We don't use `fetch` here to avoid browser-specific timeouts.
+    const ajaxRequest = new XMLHttpRequest();
+    const onDownloadPrepared = () => {
+      window.location.assign(ajaxRequest.responseURL);
+      resolve();
+    };
+    const onError = () => {
+      const status = ajaxRequest.status;
+      console.error(`GET ${downloadUrl} error: ${ajaxRequest.statusText}`);
+      if (status >= 400 && status < 500) {
+        reject("Download might have expired. Please close the tab and start new download.");
+      }
+      else {
+        reject("Download error. Please try again by reloading the page.");
+      }
+    };
+    const onCancel = () => {
+      console.error("Download was cancelled. Refresh the page to start again.");
+    };
+
+    ajaxRequest.addEventListener("load", onDownloadPrepared);
+
+    ajaxRequest.addEventListener("error", onError);
+    ajaxRequest.addEventListener("abort", onCancel);
+
+    ajaxRequest.open("GET", downloadUrl);
+    ajaxRequest.send();
   });
-
-  function beforeDownload() {
-    downloadPrepared = true;
-  }
 </script>
 
 
-{#if downloadPrepared}
+{#await prepareDownloadPromise}
+  <LoadingIndicator
+    message="Please wait: your download is being prepared. Large downloads may take a minute or two."
+  />
+
+{:then}
   <p>
     Your download is ready. You can close this tab once the download starts.
     (If you don't see a prompt to save the file, follow this <a href={downloadUrl} download="{downloadToken}.zip">download link</a>.)
   </p>
 
-{:else}
-  <LoadingIndicator
-    message="Please wait: your download is being prepared. Large downloads may take a minute or two."
-  />
+{:catch error}
+  <p>{error}</p>
 
-{/if}
+{/await}
