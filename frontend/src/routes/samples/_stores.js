2853cf87a5ff35bd6bbf7841497b5eac37fb2e33
ChadoNihi <alexandr.nil.io@gmail.com>
2022-02-04 10:04:00 +0000

    FE: Document shape of state stores

--- frontend/src/routes/samples/_stores.js
+++ frontend/src/routes/samples/_stores.js
@@ -1,5 +1,25 @@
 import { writable } from "svelte/store";
 
+
+/*
+  `distinctColumnValuesStore` has the following shape:
+  ```
+    {
+      metadata: {
+        columnName1: ["val1", "val2"],
+        columnName2: ["val1", "val2"]
+      },
+      "in silico": {
+        columnName3: ["val1", "val2"],
+        columnName4: ["val1", "val2"]
+      },
+      "qc data": {
+        columnName5: ["val1", "val2"],
+        columnName6: ["val1", "val2"]
+      }
+    }
+  ```
+*/
 // FIXME unit test
 function createDistinctColumnValuesStore() {
   const { update, subscribe } = writable({ metadata: {}, "in silico": {}, "qc data": {} });
@@ -18,6 +38,26 @@ function createDistinctColumnValuesStore() {
 
 export const distinctColumnValuesStore = createDistinctColumnValuesStore();
 
+
+/*
+  `filterStore` has the following shape:
+  ```
+    {
+      metadata: {
+        columnName1: { values: ["val1", "val2"], exclude: true },
+        columnName2: { values: ["val1", "val2"] },
+      },
+      "in silico": {
+        columnName3: { values: ["val1", "val2"] },
+        columnName4: { values: ["val1", "val2"], exclude: true },
+      },
+      "qc data": {
+        columnName5: { values: ["val1", "val2"], exclude: true },
+        columnName6: { values: ["val1", "val2"], exclude: true },
+      }
+    }
+  ```
+*/
 function createFilterStore() {
   const filterCustomStore = writable({ metadata: {}, "in silico": {}, "qc data": {} });
 
