2f59b735b937eba22a63c54b11bd67c808a4bf48
ChadoNihi <alexandr.nil.io@gmail.com>
2022-02-16 11:30:04 +0000

    FE bulk download: Reset estimate just before new estimate ...
    ... request to reduce the chance of outdated estimate being displayed in
    a form that has been "frozen" (ie disabled after clicking submit).
    See this bug ticket: https://jira.sanger.ac.uk/browse/PIJ-339

--- frontend/src/routes/samples/index.svelte
+++ frontend/src/routes/samples/index.svelte
@@ -68,6 +68,7 @@
       return;
     }
 
+    unsetDownloadEstimate();
     getBulkDownloadInfo({
       instKeyBatchDatePairs: selectedInstKeyBatchDatePairs,
       filter: { filterState: $filterStore, distinctColumnValues: $distinctColumnValuesStore },
