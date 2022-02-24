42757e030a7ef71e14b53696917ee050c17b1b60
ChadoNihi <alexandr.nil.io@gmail.com>
2022-02-24 22:28:47 +0000

    FE data viewer: Add column stores
    One store for the response data and another, "derived" store
    (ie a store that is updated automatically from its parent store)
    for storing column names for passing to the metadata request.

--- frontend/src/routes/samples/_stores.js
+++ frontend/src/routes/samples/_stores.js
@@ -1,4 +1,80 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
+
+const DEFAULT_COLUMNS = {
+  metadata: new Set([
+    "submitting_institution",
+    "public_name",
+    "host_status",
+    "selection_random",
+    "country",
+    "collection_year",
+    "host_species",
+    "isolation_source",
+    "serotype"
+  ])
+};
+const FILTER_TYPE_NONE = "none";
+
+/*
+  `columnsStore` has the following shape:
+```
+  {
+    metadata: [{
+      name: "Category Name",
+      columns: [{
+        displayName: "Column Name",
+        name: "column_name",
+        selected?: true,
+        type?: "numeric"
+      }, {
+        ...
+      }]
+    }, {
+      ...
+    }],
+    "in silico": [...],
+    "qc data": [...]
+  }
+```
+*/
+function createColumnsStore() {
+  const { set, update, subscribe } = writable();
+
+  return {
+    set,
+    subscribe,
+    setFromColumnsResponse: (columnsResponse) => set(convertColumnsResponseToState(columnsResponse))
+  };
+}
+
+export const columnsStore = createColumnsStore();
+
+
+/*
+  `columnsToDisplayStore` has the following shape:
+  ```
+  {
+    metadata: ["column_name_1", "column_name_2"],
+    "in silico": [...],
+    "qc data": [...]
+  }
+  ```
+*/
+// FIXME unit test
+export const columnsToDisplayStore = derived(columnsStore, (columnsState) =>
+  Object.keys(columnsState).reduce((accumDataTypeToColumnNames, dataType) => {
+    accumDataTypeToColumnNames[dataType] = columnsState[dataType].reduce(columnCategoryToColumnNamesReducer, []);
+    return accumDataTypeToColumnNames;
+  }, {}));
+
+function columnCategoryToColumnNamesReducer(accumColumnNames, category) {
+  category.columns.forEach((column) => {
+    if (column.selected) {
+      accumColumnNames.push(column.name);
+    }
+  });
+  return accumColumnNames;
+}
 
 
 /*
@@ -9,14 +85,8 @@ import { writable } from "svelte/store";
         columnName1: ["val1", "val2"],
         columnName2: ["val1", "val2"]
       },
-      "in silico": {
-        columnName3: ["val1", "val2"],
-        columnName4: ["val1", "val2"]
-      },
-      "qc data": {
-        columnName5: ["val1", "val2"],
-        columnName6: ["val1", "val2"]
-      }
+      "in silico": {...},
+      "qc data": {...}
     }
   ```
 */
@@ -45,17 +115,11 @@ export const distinctColumnValuesStore = createDistinctColumnValuesStore();
   ```
     {
       metadata: {
-        columnName1: { values: ["val1", "val2"], exclude: true },
-        columnName2: { values: ["val1", "val2"] },
+        columnName1: { values: ["val1", "val2"], exclude?: true },
+        columnName2: {...},
       },
-      "in silico": {
-        columnName3: { values: ["val1", "val2"] },
-        columnName4: { values: ["val1", "val2"], exclude: true },
-      },
-      "qc data": {
-        columnName5: { values: ["val1", "val2"], exclude: true },
-        columnName6: { values: ["val1", "val2"], exclude: true },
-      }
+      "in silico": {...},
+      "qc data": {...}
     }
   ```
 */
@@ -74,3 +138,45 @@ function createFilterStore() {
 }
 
 export const filterStore = createFilterStore();
+
+
+function convertColumnsResponseToState(columnsResponse) {
+  return Object.keys(columnsResponse)
+    .reduce((accumColumnsState, dataType) => {
+      accumColumnsState[dataType] = transformCategoriesFromResponse(
+        columnsResponse[dataType].categories, dataType);
+      return accumColumnsState;
+    }, {});
+}
+
+function transformCategoriesFromResponse(categories = [], dataType) {
+  return categories.reduce((accumCategories, category) => {
+    const convertedCategory = {
+      name: category.name,
+      columns: transformColumnsFromResponse(category.fields, dataType)
+    };
+    if (convertedCategory.columns.length) {
+      accumCategories.push(convertedCategory);
+    }
+    return accumCategories;
+  }, []);
+}
+
+function transformColumnsFromResponse(columns = [], dataType) {
+  return columns.reduce((accumColumns, column) => {
+    if (column.display) {
+      const convertedColumn = {
+        displayName: column["display name"],
+        name: column.name
+      };
+      if (DEFAULT_COLUMNS[dataType]?.has(column.name)) {
+        convertedColumn.selected = true;
+      }
+      if (column["filter type"] !== FILTER_TYPE_NONE) {
+        convertedColumn.type = column["filter type"];
+      }
+      accumColumns.push(convertedColumn);
+    }
+    return accumColumns;
+  }, []);
+}
