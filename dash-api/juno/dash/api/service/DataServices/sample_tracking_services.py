72cb2ce54dddb399dc9f761726d438efe2c2cfb5
ChadoNihi <alexandr.nil.io@gmail.com>
2022-03-28 18:10:19 +0100

    dash-api: Simplify key iteration, remove duplicate ...
    ... method, & fix lint errors in `DataServices/sample_tracking_services.py`

--- dash-api/juno/dash/api/service/DataServices/sample_tracking_services.py
+++ dash-api/juno/dash/api/service/DataServices/sample_tracking_services.py
@@ -65,10 +65,10 @@ class MonocleSampleTracking:
             "samples sequenced": [],
         }
         # get number of samples received and number of lanes sequenced during each month counted from "day zero"
-        for this_institution in institutions_data.keys():
+        for this_institution in institutions_data:
             # samples received
             this_institution_num_samples_received_by_date = self._num_samples_received_by_date(this_institution)
-            for this_date_string in this_institution_num_samples_received_by_date.keys():
+            for this_date_string in this_institution_num_samples_received_by_date:
                 this_date = datetime.fromisoformat(this_date_string)
                 # days_elapsed   = (this_date - self.day_zero).days
                 months_elapsed = ((this_date.year - self.day_zero.year) * 12) + (this_date.month - self.day_zero.month)
@@ -77,7 +77,7 @@ class MonocleSampleTracking:
                 ]
             # lanes sequenced
             this_institution_num_lanes_sequenced_by_date = self._num_lanes_sequenced_by_date(this_institution)
-            for this_date_string in this_institution_num_lanes_sequenced_by_date.keys():
+            for this_date_string in this_institution_num_lanes_sequenced_by_date:
                 this_date = datetime.fromisoformat(this_date_string)
                 months_elapsed = ((this_date.year - self.day_zero.year) * 12) + (this_date.month - self.day_zero.month)
                 total_num_lanes_sequenced_by_month[months_elapsed] += this_institution_num_lanes_sequenced_by_date[
@@ -215,7 +215,7 @@ class MonocleSampleTracking:
         if self.institutions_data is None:
             self.institutions_data = self.get_institutions()
         all_juno_samples = self.sample_metadata.get_samples()
-        samples = {i: [] for i in list(self.institutions_data.keys())}
+        samples = {i: [] for i in self.institutions_data}
         for this_sample in all_juno_samples:
             try:
                 this_institution_key = self.institution_db_key_to_dict[this_sample[self.sample_table_inst_key]]
@@ -261,7 +261,7 @@ class MonocleSampleTracking:
         samples_data = self.get_samples()
         institutions_data = self.get_institutions()
         sequencing_status = {}
-        for this_institution in institutions_data.keys():
+        for this_institution in institutions_data:
             sequencing_status[this_institution] = {}
             sanger_sample_id_list = [s["sanger_sample_id"] for s in samples_data[this_institution]]
             if len(sanger_sample_id_list) - 1 > 0:  # sanger_sample_id_list must be -1 to discount _ERROR entry
@@ -302,8 +302,8 @@ class MonocleSampleTracking:
         samples = self.get_samples()
         institutions_data = self.get_institutions()
         sequencing_status_data = self.get_sequencing_status()
-        batches = {i: {} for i in institutions_data.keys()}
-        for this_institution in institutions_data.keys():
+        batches = {i: {} for i in institutions_data}
+        for this_institution in institutions_data:
             # this ought to be the totalnumber of samples expected from an institution during the JUNO project
             # but currently all we know are the number of samples for which we have metadata; this will be
             # a subset of the total expected samples (until the last delivery arrives) so it isn't what we really want
@@ -317,7 +317,7 @@ class MonocleSampleTracking:
             num_samples_expected = len(samples[this_institution])
             # this is a list of the samples actually found in MLWH; it is not necessarily the same as
             # the list of sample IDs in the monocle db
-            samples_received = sequencing_status_data[this_institution].keys()
+            samples_received = sequencing_status_data[this_institution]
             # this is a dict of the number of samples received on each date; keys are YYYY-MM-DD
             num_samples_received_by_date = self._num_samples_received_by_date(this_institution)
             # work out the number of samples in each delivery
@@ -325,7 +325,7 @@ class MonocleSampleTracking:
             #             this is an approximation: one actual batch might have dates that span two (or a few?) days?
             deliveries = []
             delivery_counter = 0
-            for this_date in num_samples_received_by_date.keys():
+            for this_date in num_samples_received_by_date:
                 delivery_counter += 1
                 deliveries.append(
                     {
@@ -362,10 +362,9 @@ class MonocleSampleTracking:
 
         TODO:  improve 'failed' dict 'issue' strings
         """
-        institutions_data = self.get_institutions()
         sequencing_status_data = self.get_sequencing_status()
         status = {}
-        for this_institution in sequencing_status_data.keys():
+        for this_institution in sequencing_status_data:
             logging.debug(
                 "{}.sequencing_status_summary() received sample key pairs {} for institution {}".format(
                     __class__.__name__, sequencing_status_data[this_institution].keys(), this_institution
@@ -402,7 +401,7 @@ class MonocleSampleTracking:
                             status[this_institution]["completed"] += 1
                             # look for any failures; note one lane could have more than one failure
                             this_lane_failed = False
-                            for this_flag in self.sequencing_flags.keys():
+                            for this_flag in self.sequencing_flags:
                                 if not 1 == this_lane[this_flag]:
                                     this_lane_failed = True
                                     # record message for this failure
@@ -418,67 +417,6 @@ class MonocleSampleTracking:
                                 status[this_institution]["failed"] += 1
                             else:
                                 status[this_institution]["success"] += 1
-        return status
-
-    def pipeline_status_summary(self):
-        """
-        Returns dict with summary of the pipeline outcomes for each institution.
-
-        {  institution_1: {  'running':     <int>,
-                             'success':     <int>
-                             'failed':      <int>,
-                             'completed':   <int>,
-                             'fail_messages':  [  {  'lane':  lane_id_1,
-                                                     'stage': 'name of QC stage where issues was detected',
-                                                     'issue': 'string describing the issue'
-                                                     },
-                                                  ...
-                                                  ],
-                             },
-           institution_2...
-           }
-
-        Note that when self.pipeline_status is instantiated it creates a dataframe with
-        the data, rather than querying an API, there is no separate method to "get" pipeline
-        data and it isn't cached -- which is a bit dofferent to how institution/samples/sequencing data
-        are handled in this class.
-
-        TODO:  decide what to do about about 'failed' dict 'issue' strings
-        """
-        institutions_data = self.get_institutions()
-        sequencing_status_data = self.get_sequencing_status()
-        status = {}
-        for this_institution in institutions_data.keys():
-            if sequencing_status_data[this_institution][API_ERROR_KEY] is not None:
-                status[this_institution] = {
-                    API_ERROR_KEY: "Server Error: Records cannot be collected at this time. Please try again later."
-                }
-                continue
-            status[this_institution] = {
-                API_ERROR_KEY: None,
-                "running": 0,
-                "completed": 0,
-                "success": 0,
-                "failed": 0,
-                "fail_messages": [],
-            }
-            sanger_sample_id_list = sequencing_status_data[this_institution].keys()
-            for this_sanger_sample_id in sanger_sample_id_list:
-                if this_sanger_sample_id == API_ERROR_KEY:
-                    continue
-                # if a sample is in MLWH but there are no lane data, it means sequencing hasn't been done yet
-                # i.e. only samples with lanes need to be looked at by the lines below
-                for this_lane in this_sequencing_status_data[this_sanger_sample_id]["lanes"]:
-                    this_lane_completed, this_lane_success, fail_messages = self.get_sequencing_outcome_for_lane(
-                        this_sanger_sample_id, this_lane
-                    )
-                    if this_lane_completed:
-                        status[this_institution]["completed"] += 1
-                    if this_lane_success:
-                        status[this_institution]["success"] += 1
-                    else:
-                        status[this_institution]["failed"] += 1
-                        status[this_institution]["fail_messages"] += fail_messages
         return status
 
     def get_sequencing_outcome_for_lane(self, this_sample_id, this_lane):
@@ -564,7 +502,7 @@ class MonocleSampleTracking:
                 "failed": 0,
                 "fail_messages": [],
             }
-            sanger_sample_id_list = sequencing_status_data[this_institution].keys()
+            sanger_sample_id_list = sequencing_status_data[this_institution]
             for this_sanger_sample_id in sanger_sample_id_list:
                 if this_sanger_sample_id == API_ERROR_KEY:
                     continue
@@ -618,7 +556,7 @@ class MonocleSampleTracking:
 
     def _num_samples_received_by_date(self, institution):
         sequencing_status_data = self.get_sequencing_status()
-        samples_received = sequencing_status_data[institution].keys()
+        samples_received = sequencing_status_data[institution]
         if sequencing_status_data[institution][API_ERROR_KEY] is not None:
             return {}
         num_samples_received_by_date = defaultdict(int)
@@ -635,7 +573,7 @@ class MonocleSampleTracking:
         sequencing_status_data = self.get_sequencing_status()
         if sequencing_status_data[institution][API_ERROR_KEY] is not None:
             return {}
-        samples_received = sequencing_status_data[institution].keys()
+        samples_received = sequencing_status_data[institution]
         num_lanes_sequenced_by_date = defaultdict(int)
 
         for this_sanger_sample_id in samples_received:
