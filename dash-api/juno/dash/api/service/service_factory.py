7e1b5fb104e2237fb55b4e0eca8de94a9ba65863
ts24 <ts24@sanger.ac.uk>
2022-03-30 11:49:25 +0000

    make dash.api.service.DataSources.user_data.UserAuthentication available via dashboatd API service factory (the Connexion wrapper gubbins)

--- dash-api/juno/dash/api/service/service_factory.py
+++ dash-api/juno/dash/api/service/service_factory.py
@@ -1,6 +1,14 @@
 from dash.api.service.DataServices.sample_data_services     import MonocleSampleData
 from dash.api.service.DataServices.sample_tracking_services import MonocleSampleTracking
-from dash.api.service.DataServices.user_services            import MonocleUser
+from dash.api.service.DataServices.user_services            import MonocleAuthentication, MonocleUser
+
+
+class AuthenticationService(MonocleAuthentication):
+    """ Wrapper class for MonocleAuthentication """
+
+    # this is a placeholder
+    # currently this wrapper needs no specific initlialization
+
 
 class UserService(MonocleUser):
     """ Wrapper class for MonocleUser """
@@ -48,6 +56,10 @@ class ServiceFactory:
     """ Instantiate a service """
 
     TEST_MODE = False
+
+    @staticmethod
+    def authentication_service() -> AuthenticationService:
+        return AuthenticationService()
 
     @staticmethod
     def user_service(username: str) -> UserService:
