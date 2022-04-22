cc4996f122ac6022fb78ab38caf3a826d309a179
ChadoNihi <alexandr.nil.io@gmail.com>
2022-04-22 11:55:02 +0100

    FE: Update svelte & sveltekit and fix breaking changes

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,11 +5,8 @@ const production = process.env.NODE_ENV === "production" || process.env.NODE_ENV
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // hydrate the <div id="app"> element in src/app.html
-    target: "#app",
     // `appDir` should be the same as the route in `proxy/nginx.prod.proxy.conf`:
     appDir: "_app",
-    ssr: production,
     adapter: nodeAdapter({
       precompress: production
     })
