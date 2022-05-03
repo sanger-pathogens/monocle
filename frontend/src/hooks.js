c4cf2f55c5281a2400d02eb59beafb8364fb9366
ChadoNihi <alexandr.nil.io@gmail.com>
2022-05-03 15:10:20 +0100

    FE: Update all deps & fix breaking changes

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -1,6 +1,13 @@
 const DASHBOARD_API_INTERNAL_URL = "http://dash-api:5000/dashboard-api";
 const MONOCLE_URL = "http://monocle.pam.sanger.ac.uk";
 const DASHBOARD_API_URL = `${MONOCLE_URL}/dashboard-api`;
+const OPTIONS_RESOLVE = Object.freeze({ ssr: false });
+
+
+// FIXME add unit test
+export async function handle({ event, resolve }) {
+  return resolve(event, OPTIONS_RESOLVE);
+}
 
 // This function allows us to modify a fetch request that happens in the `load` function
 // for the server. See more: // https://kit.svelte.dev/docs#hooks-serverfetch
