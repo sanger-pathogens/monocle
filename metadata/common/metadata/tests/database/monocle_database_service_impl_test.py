71ee3c5da492cf03a6d95a12652eca8cc0069640
Magnus Manske <mm6@sanger.ac.uk>
2022-05-11 14:00:40 +0100

    attempting live SQL generation

--- metadata/common/metadata/tests/database/monocle_database_service_impl_test.py
+++ metadata/common/metadata/tests/database/monocle_database_service_impl_test.py
@@ -29,8 +29,8 @@ class TestMonocleDatabaseServiceImpl(unittest.TestCase):
             from metadata.wsgi import application
 
             self.assertIsNotNone(application)
-
             connector_mock.application = application
+
             self.connector = connector_mock
             self.connection = Mock()
             self.transactional_connection = Mock()
