4f0054ada2403f05fe96a0e8521449f0309ca90e
Elisabetta Galatola <eg19@sanger.ac.uk>
2022-05-26 16:20:13 +0100

    WIP: add gps institutions

--- database/institutions.sql
+++ database/institutions.sql
@@ -75,3 +75,6 @@ values ('Istituto Superiore di Sanità', 'Italy', 41.904144, 12.51806);
 insert into api_institution (name, country, latitude, longitude)
 values ('Toros University', 'Turkey', 36.790048, 34.593931);
 
+-- GPS institutions
+
+
