b8b9bc6ff7a25cfdd392f22a202fb9cc3a4bfe6a
Magnus Manske <mm6@sanger.ac.uk>
2022-06-16 09:16:26 +0100

    trying to query project into from metadata API

--- dash-api/dash/api/routes.py
+++ dash-api/dash/api/routes.py
@@ -115,7 +115,7 @@ def sequencing_status_summary_route():
 
 def interface_route():
     """Get interface information"""
-    project_data = ServiceFactory.sample_data_service(get_authenticated_username()).project_information()
+    project_data = ServiceFactory.sample_tracking_service(get_authenticated_username()).project_information()
     response_dict = {"project": project_data}
     return call_jsonify(response_dict), HTTPStatus.OK
 
