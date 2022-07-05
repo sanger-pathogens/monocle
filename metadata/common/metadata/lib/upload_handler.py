ee61926c6693c00e617fa1c2213d6c8437fb6856
Elisabetta Galatola <eg19@sanger.ac.uk>
2022-07-05 11:16:10 +0100

    Add missing 'application' object to constructor

--- metadata/common/metadata/lib/upload_handler.py
+++ metadata/common/metadata/lib/upload_handler.py
@@ -44,6 +44,7 @@ class UploadHandler:
         self.__spreadsheet_def = in_def
         self.__do_validation = do_validation
         self.__institutions = None
+        self.application = application
         # validation will fail if file extension isn't as expected, when this flag is true
         self.check_file_extension = True
         self.file_delimiter = "\t"
