278af5f23fd89bcf7dfa47a8304f211ed9eb8e7e
ts24 <ts24@sanger.ac.uk>
2022-07-15 10:18:43 +0000

    refactor data view wrapper script (runs python script in docker container) so project-specific values now appear in only two global vars

--- data_view/bin/run_data_view_script_in_docker.sh
+++ data_view/bin/run_data_view_script_in_docker.sh
@@ -6,7 +6,7 @@
 # 
 # Volume mounts required are:
 # - the s3 bucket (source data) directory as SAMPLE_DATA_PATH
-# - the output directory as `/app/monocle_juno_institution_view`
+# - the output ("insttitution view") directory as `/app/${INST_VIEW_SUBDIR}`
 # - the db config file as `/app/my.cnf`
 # - the MLWH API config file as `/app/mlwh-api.yml`
 # - the script itself, in `/app`
@@ -15,20 +15,23 @@
 # - must be attached to the Monocle docker network (see `docker-compose.yml`);
 #   docker compose names this after the user that starts it
 # 
-# The s3 bucket is mounted as SAMPLE_DATA_PATH to that target of the symlinks
+# The s3 bucket is mounted as SAMPLE_DATA_PATH so that target of the symlinks
 # has the same path within the container as on the host machine.
 # 
 # Any arguments provided will be passed to the script
 
-SAMPLE_DATA_PATH="${HOME}/monocle_juno"
+SAMPLE_DATA_SUBDIR="monocle_juno"
+SAMPLE_DATA_PATH="${HOME}/${SAMPLE_DATA_SUBDIR}"
+INST_VIEW_SUBDIR="monocle_juno_institution_view"
+INST_VIEW_PATH="${HOME}/${INST_VIEW_SUBDIR}"
 if ! docker run  -u `id -u`:`id -g` \
             --rm \
-            --volume ${SAMPLE_DATA_PATH}:${SAMPLE_DATA_PATH}  \
-            --volume `pwd`/monocle_juno_institution_view:/app/monocle_juno_institution_view  \
+            --volume ${SAMPLE_DATA_PATH}:/home/<USER>/${SAMPLE_DATA_SUBDIR}  \
+            --volume ${INST_VIEW_PATH}:/app/${INST_VIEW_SUBDIR}  \
             --volume `pwd`/my.cnf:/app/my.cnf \
             --volume `pwd`/mlwh-api.yml:/app/mlwh-api.yml \
             --volume `pwd`/create_download_view_for_sample_data.py:/app/create_download_view_for_sample_data.py \
-            --env "MONOCLE_DATA=/home/<USER>/monocle_juno" \
+            --env "MONOCLE_DATA=/home/<USER>/${SAMPLE_DATA_SUBDIR}" \
             --name "create_download_view_for_sample_data_$$" \
             --network <USER>_default \
             gitlab-registry.internal.sanger.ac.uk/sanger-pathogens/monocle/monocle-dash-api:<DOCKERTAG> \
@@ -42,7 +45,7 @@ fi
 # Add MD5 checksum files to each lane after all the directories have been
 # created.   Doing this here should ensure that any new lanes/data get MD5
 # checksum files added asap.
-MD5_ROOT_DIR="${HOME}/monocle_juno_institution_view"
+MD5_ROOT_DIR="${INST_VIEW_PATH}"
 DOWNLOADS_DIR="${MD5_ROOT_DIR}/downloads"
 # be careful with this for loop:  DIR may include a space so patterns like
 # `for DIR $(ls pattern | some | pipe)` won't work as expected
