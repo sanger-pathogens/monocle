0077a4bbf385de9cacf93ae3017b007f01ad72a1
Magnus Manske <mm6@sanger.ac.uk>
2022-07-21 15:39:51 +0100

    misc fixes

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -16,11 +16,10 @@ export const HTTP_STATUS_CODE_UNAUTHORIZED = 401;
 
 // IMPORTANT: when incrementing this key, put the old one to `LOCAL_STORAGE_KEYS_OLD_COLUMNS_STATE`. This is
 // needed to clear `localStorage` from old data.
-export const LOCAL_STORAGE_KEY_COLUMNS_STATE = "columnsState_3";
+export const LOCAL_STORAGE_KEY_COLUMNS_STATE = "columnsState_2";
 export const LOCAL_STORAGE_KEYS_OLD_COLUMNS_STATE = [
   "columnsState",
   "columnsState_1",
-  "columnsState_2",
 ];
 
 export const MIME_TYPE_HTML = "text/html";
