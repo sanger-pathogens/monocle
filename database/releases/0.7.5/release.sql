cf526beed2fc278dd26c550339d78e19cf400009
ts24 <ts24@sanger.ac.uk>
2022-08-15 09:13:43 +0000

    update db release tag from 0.7.4 to 0.7.5

--- database/releases/0.7.5/release.sql
+++ database/releases/0.7.5/release.sql
@@ -4,4 +4,4 @@
 
 ALTER TABLE `qc_data` MODIFY `QC_pipeline_version` VARCHAR(256) NOT NULL;
 
-CALL update_database_version('0.7.4', 'bug fix for JUNO QC data');
+CALL update_database_version('0.7.5', 'bug fix for JUNO QC data');
