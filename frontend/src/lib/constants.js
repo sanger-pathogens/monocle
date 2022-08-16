e8cbab094f68f5f1fb144c4b5626dbeac3a303d9
ts24 <ts24@sanger.ac.uk>
2022-08-16 09:10:13 +0000

    Tweak JUNO QC data column attributes, so that only the overal QC status (`status`) is displayed by default in the data viewer

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -16,7 +16,7 @@ export const HTTP_STATUS_CODE_UNAUTHORIZED = 401;
 
 // IMPORTANT: when incrementing this key, put the old one to `SESSION_STORAGE_KEYS_OLD_COLUMNS_STATE`. This is
 // needed to clear `sessionStorage` from old data (otherwise it's cleared only on closing the browser tab/window).
-export const SESSION_STORAGE_KEY_COLUMNS_STATE = "columnsState_7";
+export const SESSION_STORAGE_KEY_COLUMNS_STATE = "columnsState_8";
 export const SESSION_STORAGE_KEYS_OLD_COLUMNS_STATE = [
   "columnsState",
   "columnsState_1",
@@ -25,6 +25,7 @@ export const SESSION_STORAGE_KEYS_OLD_COLUMNS_STATE = [
   "columnsState_4",
   "columnsState_5",
   "columnsState_6",
+  "columnsState_7",
 ];
 
 export const MIME_TYPE_HTML = "text/html";
