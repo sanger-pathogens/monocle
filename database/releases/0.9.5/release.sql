be44ee911e834bc9744278e98b3c64615b5eef9d
Elisabetta Galatola <eg19@sanger.ac.uk>
2022-08-31 14:46:42 +0100

    Fix typo in release comment

--- database/releases/0.9.5/release.sql
+++ database/releases/0.9.5/release.sql
@@ -2,7 +2,7 @@
 # Increase varchar size for JUNO variants (in-silico table)
 # **************************************************************
 
--- Add missing CHECK statement to Hetsites_50bp data type (gps_qc_data table)
+-- Amend VARCHAR size
 ALTER TABLE `in_silico` MODIFY `twenty_three_S1_variant` VARCHAR(256);
 ALTER TABLE `in_silico` MODIFY `twenty_three_S3_variant` VARCHAR(256);
 ALTER TABLE `in_silico` MODIFY `GYRA_variant` VARCHAR(256);
